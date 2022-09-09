0e27352107908b0a10e5993227d761f592efe54d
MonsterDruide1 <5958456@gmail.com>
2022-09-09 17:11:30 +0200

    `hid`: Add NpadStates

--- include/nn/hid.h
+++ include/nn/hid.h
@@ -7,6 +7,7 @@
 
 #include <nn/types.h>
 #include <nn/util.h>
+#include <nn/util/util_BitFlagSet.h>
 
 namespace nn {
 namespace hid {
@@ -31,13 +32,44 @@ struct ControllerSupportResultInfo {
     int mSelectedId;
 };
 
+struct AnalogStickState {
+    int32_t x, y;
+};
+
+struct NpadButton;
+struct NpadAttribute;
+struct NpadId;
+
+typedef nn::util::BitFlagSet<32, NpadAttribute> NpadAttributesSet;
+typedef nn::util::BitFlagSet<64, NpadButton> NpadButtonSet;
+typedef nn::util::BitFlagSet<32, NpadStyleTag> NpadStyleSet;
+typedef uint32_t NpadIdType;
+
+struct NpadBaseState {
+    int64_t samplingNumber;
+    NpadButtonSet buttons;
+    AnalogStickState analogStickL;
+    AnalogStickState analogStickR;
+    NpadAttributesSet attributes;
+};
+
+struct NpadHandheldState : NpadBaseState {};
+struct NpadJoyDualState : NpadBaseState {};
+struct NpadFullKeyState : NpadBaseState {};
+struct NpadJoyLeftState : NpadBaseState {};
+struct NpadJoyRightState : NpadBaseState {};
+
 void InitializeNpad();
-void SetSupportedNpadIdType(u32 const*, u64);
-void SetSupportedNpadStyleSet(nn::util::BitFlagSet<32, nn::hid::NpadStyleTag>);
-void GetNpadStyleSet(u32 const&);
-void GetNpadStates(nn::hid::NpadHandheldState*, s32, u32 const&);
-static int ShowControllerSupport(nn::hid::ControllerSupportResultInfo*,
-                                 ControllerSupportArg const&);
+void SetSupportedNpadIdType(const u32*, u64);
+void SetSupportedNpadStyleSet(NpadStyleSet);
+void GetNpadStyleSet(const u32&);
+static int ShowControllerSupport(ControllerSupportResultInfo*, const ControllerSupportArg&);
+
+void GetNpadStates(NpadHandheldState*, int, const u32&);
+void GetNpadStates(NpadJoyDualState*, int, const u32&);
+void GetNpadStates(NpadFullKeyState*, int, const u32&);
+void GetNpadStates(NpadJoyLeftState*, int, const u32&);
+void GetNpadStates(NpadJoyRightState*, int, const u32&);
 
 }  // namespace hid
 }  // namespace nn
