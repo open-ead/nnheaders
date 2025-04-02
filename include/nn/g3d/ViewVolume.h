be999e6e8e80746a890be1d4fe4bcc88d48c62c1
GRAnimated <devin@nhscan.com>
2025-04-02 18:14:36 -0400

    Return types

--- include/nn/g3d/ViewVolume.h
+++ include/nn/g3d/ViewVolume.h
@@ -15,10 +15,10 @@ public:
     void SetPerspective(f32, f32, f32, f32, const nn::util::neon::MatrixRowMajor4x3fType&);
     void SetFrustum(f32, f32, f32, f32, f32, f32, const nn::util::neon::MatrixRowMajor4x3fType&);
     void SetOrtho(f32, f32, f32, f32, f32, f32, const nn::util::neon::MatrixRowMajor4x3fType&);
-    void TestIntersection(const nn::g3d::Sphere&);
-    void TestIntersectionEx(const nn::g3d::Sphere&);
-    void TestIntersection(const nn::g3d::Aabb&);
-    void TestIntersectionEx(const nn::g3d::Aabb&);
+    bool TestIntersection(const nn::g3d::Sphere&);
+    bool TestIntersectionEx(const nn::g3d::Sphere&);
+    bool TestIntersection(const nn::g3d::Aabb&);
+    bool TestIntersectionEx(const nn::g3d::Aabb&);
 
 private:
     void* filler[29];
