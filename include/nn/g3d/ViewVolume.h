372156b0996685e44e82a88ef8c9dc3969c53d33
GRAnimated <devin@nhscan.com>
2025-04-02 18:56:05 -0400

    Fix

--- include/nn/g3d/ViewVolume.h
+++ include/nn/g3d/ViewVolume.h
@@ -16,9 +16,9 @@ public:
     void SetFrustum(f32, f32, f32, f32, f32, f32, const nn::util::neon::MatrixRowMajor4x3fType&);
     void SetOrtho(f32, f32, f32, f32, f32, f32, const nn::util::neon::MatrixRowMajor4x3fType&);
     bool TestIntersection(const nn::g3d::Sphere&);
-    bool TestIntersectionEx(const nn::g3d::Sphere&);
+    s32 TestIntersectionEx(const nn::g3d::Sphere&);
     bool TestIntersection(const nn::g3d::Aabb&);
-    bool TestIntersectionEx(const nn::g3d::Aabb&);
+    s32 TestIntersectionEx(const nn::g3d::Aabb&);
 
 private:
     void* filler[29];
