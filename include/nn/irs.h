a3b92cd881ff609843a3ff514ec56798fa0d773c
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-12-27 18:43:10 +0100

    add:irs functions

---
+++ include/nn/irs.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <nn/types.h>
+
+namespace nn::irsensor {
+struct ClusteringProcessorConfig;
+struct ClusteringProcessorState;
+struct DpdProcessorConfig;
+struct DpdProcessorPointingState;
+struct DpdProcessorState;
+struct HandAnalysisConfig;
+struct ImageTransferProcessorConfig;
+struct ImageTransferProcessorState;
+struct MomentProcessorConfig;
+struct MomentProcessorState;
+struct TeraPluginProcessorConfig;
+struct TeraPluginProcessorState;
+
+enum class IrCameraStatus : u32 {
+    Available,
+    Unsupported,
+    Unconnected,
+};
+
+struct IrCameraHandle {
+    u8 playerNumber;
+    u8 deviceType;
+    u8 reserved[2];
+};
+
+struct Rect {
+    u16 x = 0;
+    u16 y = 0;
+    u16 width = 320;
+    u16 height = 240;
+};
+
+IrCameraHandle GetIrCameraHandle(const u32&);
+void Initialize(const IrCameraHandle& handle);
+void Finalize(const IrCameraHandle& handle);
+IrCameraStatus GetIrCameraStatus(const IrCameraHandle& handle);
+void StopImageProcessor(const IrCameraHandle& handle);
+
+void GetMomentProcessorDefaultConfig(MomentProcessorConfig* outConfig);
+void RunMomentProcessor(const IrCameraHandle& handle, const MomentProcessorConfig& config);
+Result GetMomentProcessorState(MomentProcessorState* outState, const IrCameraHandle& handle);
+Result GetMomentProcessorStates(MomentProcessorState* outStates, s32* outCount, s32,
+                                const IrCameraHandle& handle);
+void* CalculateMomentRegionStatistic(const MomentProcessorState* state, const Rect&, s32, s32, s32,
+                                     s32);  // TODO: change return type
+
+void GetClusteringProcessorDefaultConfig(ClusteringProcessorConfig* outConfig);
+void RunClusteringProcessor(const IrCameraHandle& handle, const ClusteringProcessorConfig& config);
+Result GetClusteringProcessorState(ClusteringProcessorState* outState,
+                                   const IrCameraHandle& handle);
+Result GetClusteringProcessorStates(ClusteringProcessorState* outStates, s32* outCount, s32,
+                                    const IrCameraHandle& handle);
+
+void GetImageTransferProcessorDefaultConfig(ImageTransferProcessorConfig* outConfig);
+void RunImageTransferProcessor(const IrCameraHandle& handle,
+                               const ImageTransferProcessorConfig& config, void*, u64);
+Result GetImageTransferProcessorState(ImageTransferProcessorState* outState, void*, u64,
+                                      const IrCameraHandle& handle);
+
+Result RunHandAnalysis(const IrCameraHandle& handle, const HandAnalysisConfig& config);
+
+Result RunTeraPluginProcessor(const IrCameraHandle& handle,
+                              const TeraPluginProcessorConfig& config);
+
+void RunDpdProcessor(const IrCameraHandle& handle);
+void GetDpdProcessorDefaultConfig(DpdProcessorConfig* outConfig);
+void RunDpdProcessor(const IrCameraHandle& handle, const DpdProcessorConfig& config);
+Result GetDpdProcessorStates(DpdProcessorPointingState* outStates, s32* outCount, s32,
+                             const IrCameraHandle& handle);
+Result GetDpdProcessorStates(DpdProcessorState* outStates, s32* outCount, s32,
+                             const IrCameraHandle& handle);
+
+Result GetTeraPluginProcessorStates(TeraPluginProcessorState* outStates, s32* outCount, s32, long,
+                                    u32, s32, const IrCameraHandle& handle);
+
+Rect MakeRect(s32 x, s32 y, s32 width, s32 height);
+}  // namespace nn::irsensor
