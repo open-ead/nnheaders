cc88e7f95e535cd750e2879288b8318f0478d863
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-12-28 14:59:52 +0100

    add:MomentProcessorConfig and MomentProcessorState

--- include/nn/irs/MomentProcessorConfig.h
+++ include/nn/irs/MomentProcessorConfig.h
@@ -1,15 +1,17 @@
 #pragma once
 
-#include <nn/irs/ImageTransferProcessorFormat.h>
+#include <nn/irs/Rect.h>
 #include <nn/types.h>
 
 namespace nn::irsensor {
-struct __attribute__((packed)) ImageTransferProcessorConfig {
+struct MomentProcessorConfig {
     s64 exposureTime = 300000;
     s32 lightTarget = 0;
     s32 gain = 8;
     bool isNegativeImageUsed = false;
     u8 reserved[7];
-    ImageTransferProcessorFormat format = ImageTransferProcessorFormat::_320x240;
+    Rect windowOfInterest;
+    s32 preprocess = 1;
+    s32 preprocessIntensityThreshold = 80;
 };
 }  // namespace nn::irsensor
