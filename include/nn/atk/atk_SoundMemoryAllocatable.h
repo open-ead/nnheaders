6fe0fbb40abeaca2a0ffbb91a773eb69551846d8
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-19 20:05:48 -0300

    atk: Define `SoundMemoryAllocatable`

---
+++ include/nn/atk/atk_SoundMemoryAllocatable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <nn/types.h>
+
+namespace nn::atk {
+class SoundMemoryAllocatable {
+public:
+    typedef void(*DisposeCallback)(void*);
+
+    virtual ~SoundMemoryAllocatable() = 0;
+    virtual void* Allocate(size_t size) = 0;
+    virtual void* Allocate(size_t size, DisposeCallback callback, void* callbackArg) = 0;
+    virtual void* GetAllocateSize(size_t size, bool needMemoryPool) = 0;
+};
+} // namespace nn::atk
