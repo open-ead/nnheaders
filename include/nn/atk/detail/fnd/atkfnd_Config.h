b39dca476f5db6674c8bbbb9c0b258e9ddb5e734
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-20 10:42:10 -0300

    atk: Define some of `SoundArchive`
    Only defined member variables and virtual functions, not general functions

---
+++ include/nn/atk/detail/fnd/atkfnd_Config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <nn/types.h>
+
+namespace nn::atk::detail::fnd {
+using position_t = std::ptrdiff_t;
+} // namespace nn::atk::detail::fnd
---
+++ include/nn/atk/detail/fnd/atkfnd_Stream.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace nn::atk::detail::fnd {
+class Stream {
+public:
+    virtual ~Stream();
+};
+} // namespace nn::atk::detail::fnd
