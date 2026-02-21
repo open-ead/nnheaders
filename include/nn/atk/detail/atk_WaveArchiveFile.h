2af3d30f74391fc0a5c4cf8bd796e98a95668077
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-21 10:24:01 -0300

    atk: Define `detail::WaveArchiveFile` and its member structs

---
+++ include/nn/atk/detail/atk_WaveArchiveFile.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <nn/atk/detail/atk_Util.h>
+
+namespace nn::atk::detail {
+struct WaveArchiveFile {
+    struct InfoBlock;
+    struct FileBlock;
+    struct FileHeader : BinaryFileHeader {
+        Util::ReferenceWithSize toBlocks[2];
+
+        InfoBlock* GetInfoBlock();
+        u32 GetInfoBlockOffset();
+
+        FileBlock* GetFileBlock();
+        u32 GetFileBlockOffset();
+    };
+    static_assert(sizeof(FileHeader) == 0x2C);
+
+    struct FileBlockBody { /* Unknown structure */ };
+    struct FileBlock {
+        BinaryBlockHeader header;
+        FileBlockBody body;
+    };
+
+    struct InfoBlockBody {
+        Util::ReferenceWithSizeTable table;
+    };
+
+    struct InfoBlock {
+        BinaryBlockHeader header;
+        InfoBlockBody body;
+    };
+};
+} // namespace nn::atk::detail
