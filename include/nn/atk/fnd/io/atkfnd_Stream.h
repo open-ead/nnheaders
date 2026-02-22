9bf018f52b7b3b9b97daf30d7133cdf20ace9f0d
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-22 12:55:43 -0300

    atk: Add virtual methods to `detail::fnd::Stream`

--- include/nn/atk/fnd/io/atkfnd_Stream.h
+++ include/nn/atk/fnd/io/atkfnd_Stream.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <nn/types.h>
+
+#include <nn/atk/fnd/basis/atkfnd_Config.h>
+#include <nn/atk/fnd/basis/atkfnd_Result.h>
+
 namespace nn::atk::detail::fnd {
 class Stream {
 public:
@@ -10,6 +15,20 @@ public:
     };
 
     virtual ~Stream();
+    
+    virtual void Close() = 0;
+    virtual bool IsOpened() const = 0;
+    
+    virtual std::size_t Read(void* buf, std::size_t length, FndResult* result) = 0;
+    virtual std::size_t Write(void* buf, std::size_t length, FndResult* result) = 0;
+    virtual FndResult Seek(position_t offset, SeekOrigin origin) = 0;
+    
+    virtual position_t GetCurrentPosition() const = 0;
+    virtual std::size_t GetSize() const = 0;
+    
+    virtual bool CanRead() const = 0;
+    virtual bool CanWrite() const = 0;
+    virtual bool CanSeek() const = 0;
 };
 static_assert(sizeof(Stream) == 0x8);
 } // namespace nn::atk::detail::fnd
