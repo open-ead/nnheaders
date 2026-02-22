0dce63a3cbfe21e2f1696a308677d4f5da637b98
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-22 11:07:13 -0300

    atk: Define enums in `detail::fnd::Stream` and `detail::fnd::FileStream`

--- include/nn/atk/fnd/io/atkfnd_FileStream.h
+++ include/nn/atk/fnd/io/atkfnd_FileStream.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <nn/atk/fnd/io/atkfnd_Stream.h>
+
 namespace nn::atk::detail::fnd {
-class FileStream {
+class FileStream : Stream {
 public:
-    virtual ~FileStream();
+    enum AccessMode {
+        AccessMode_None,
+        AccessMode_Read,
+        AccessMode_Write,
+        AccessMode_ReadAndWrite,
+        AccessMode_AllowAppend,
+        AccessMode_AllowAppendAndWrite = 6,
+    };
+
+    ~FileStream() override;
 };
+static_assert(sizeof(FileStream) == 0x8);
 } // namespace nn::atk::detail::fnd
--- include/nn/atk/fnd/io/atkfnd_Stream.h
+++ include/nn/atk/fnd/io/atkfnd_Stream.h
@@ -3,6 +3,13 @@
 namespace nn::atk::detail::fnd {
 class Stream {
 public:
+    enum SeekOrigin {
+        SeekOrigin_Begin,
+        SeekOrigin_End,
+        SeekOrigin_Current,
+    };
+
     virtual ~Stream();
 };
+static_assert(sizeof(Stream) == 0x8);
 } // namespace nn::atk::detail::fnd
