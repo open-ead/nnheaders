1163056d2b2690ab25f43e11c8004ff4b46ecfe0
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-22 20:08:47 -0300

    atk: Define `detail::VolumeThroughModePacket`

---
+++ include/nn/atk/detail/atk_VolumeThroughModePacket.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <nn/atk/detail/atk_ValueArray.h>
+
+namespace nn::atk::detail {
+class VolumeThroughModePacket {
+public:
+    using VolumeThroughModeArray = ValueArray<u8>;
+
+    static std::size_t GetRequiredMemSize(s32 busCount);
+
+    bool Initialize(void* buffer, std::size_t size, s32 busCount);
+
+    void Reset();
+
+    VolumeThroughModePacket& operator=(const VolumeThroughModePacket& rhs);
+
+private:
+    VolumeThroughModeArray m_VolumeThroughMode;
+    bool m_IsVolumeThroughModeUsed;
+    f32 m_BinaryVolume;
+};
+static_assert(sizeof(VolumeThroughModePacket) == 0x18);
+} // namespace nn::atk::detail
