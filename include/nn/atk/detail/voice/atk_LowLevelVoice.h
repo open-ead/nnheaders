0b1ad68d7e8947e48d4f43213b58ebaabd200461
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-22 21:39:13 -0300

    atk: Define `detail::LowLevelVoice` and `detail::LowLevelVoiceAllocator`

---
+++ include/nn/atk/detail/voice/atk_LowLevelVoice.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <atomic>
+
+#include <nn/audio/audio_VoiceTypes.h>
+#include <nn/audio/audio_Common.h>
+
+#include <nn/atk/atk_Adpcm.h>
+#include <nn/atk/atk_Global.h>
+#include <nn/atk/detail/atk_Config.h>
+#include <nn/atk/submix/atk_OutputReceiver.h>
+
+namespace nn::atk::detail {
+class Voice;
+
+class alignas(256) LowLevelVoice {
+public:
+    void FreeAllWaveBuffer();
+    
+    bool IsAvailable() const;
+    void SetAvailable(bool isAvailable);
+
+    bool IsVoiceDroppedFlagOn() const;
+
+    void AppendWaveBuffer(WaveBuffer* waveBuffer);
+
+    void UpdateState(OutputMode outputMode);
+    void UpdateStatePlay(bool isRun, OutputMode outputMode);
+    void UpdateStatePause(bool isRun, OutputMode outputMode);
+
+    void SetPriority(s32 priority);
+
+    bool AllocVoice();
+
+    void SetState(VoiceState state);
+
+    void UpdateVoiceInfo(VoiceInfo* voiceInfo) const;
+    void UpdateVoiceParam(const VoiceParam& voiceParam, OutputMode outputMode);
+    void UpdateVolume(const VoiceParam& voiceParam);
+    void UpdatePitch(const VoiceParam& voiceParam);
+    void UpdateBiquadFilter(const VoiceParam& voiceParam);
+    void UpdateLowPassFilter(const VoiceParam& voiceParam);
+    void UpdateMixVolume(const OutputMix& outputMix, OutputMode outputMode);
+    void UpdateWaveBuffer(bool isRun, OutputMode outputMode);
+    void UpdatePlayPosition();
+    void UpdateWaveBufferOnPlayState();
+    void UpdateWaveBufferOnStopState(OutputMode outputMode);
+
+    bool AppendWaveBufferToVoice(WaveBuffer* waveBuffer);
+
+    void UpdateMixVolumeOnSubMix(const OutputMix& outputMix, OutputMode outputMode);
+    void UpdateMixVolumeOnFinalMix(const OutputMix& outputMix, OutputMode outputMode);
+
+    void SetVoiceMixVolume(f32, s32);
+
+    static f32 GetClampedVoiceVolume(f32);
+
+private:
+    AdpcmParam m_AdpcmParam;
+    audio::VoiceType m_Voice;
+    bool m_IsAvailable;
+    bool m_IsSetVoiceSlot;
+    VoiceParam m_VoiceParam;
+    s32 m_Priority;
+    VoiceState m_State;
+    u32 m_SampleRate;
+    SampleFormat m_SampleFormat;
+    position_t m_PlayPosition;
+    OutputReceiver* m_pOutputReceiver;
+    WaveBuffer* m_WaveBufferListBegin;
+    WaveBuffer* m_WaveBufferListEnd;
+    WaveBuffer* m_LastAppendBuffer;
+    Voice* m_pVoice;
+    audio::NodeId nodeId;
+};
+static_assert(sizeof(LowLevelVoice) == 0x100);
+
+class LowLevelVoiceAllocator {
+public:
+    LowLevelVoiceAllocator();
+
+    std::size_t GetRequiredMemSize(s32 voiceCount);
+
+    void Initialize(s32 voiceCount, void* mem, std::size_t memSize);
+    void Finalize();
+
+    void UpdateAllVoiceState(OutputMode outputMode);
+
+    LowLevelVoice* AllocVoice();
+    u64 GetVoiceArrayIndex(LowLevelVoice* pVoice);
+    void FreeVoice(LowLevelVoice* pVoice);
+
+    s32* GetDroppedVoiceCount();
+
+private:
+    void* m_pVoiceArray{};
+    LowLevelVoice** m_ppVoiceTable{};
+    s32 m_UsingCount{0};
+    s32* m_pAssignedTableIndex{};
+    s32 m_VoiceCount{};
+    std::atomic<s32> m_DroppedVoiceCount{0};
+};
+static_assert(sizeof(LowLevelVoiceAllocator) == 0x28);
+} // namespace nn::atk::detail
