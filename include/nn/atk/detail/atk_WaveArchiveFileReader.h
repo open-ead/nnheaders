ede2eb42c35382884b372b13e9cb830774ae227a
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-23 13:53:42 -0300

    atk: Define `detail::WaveArchiveFileReader`

---
+++ include/nn/atk/detail/atk_WaveArchiveFileReader.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <nn/atk/detail/atk_WaveArchiveFile.h>
+
+namespace nn::atk::detail {
+class WaveArchiveFileReader {
+public:
+    constexpr static s32 SignatureFile = 0x52415746; // FWAR
+    constexpr static s32 SignatureWarcTable = 0x54415746; //FWAT
+
+    struct IndividualLoadTable {
+        void* waveFile[1];
+    };
+
+    WaveArchiveFileReader();
+    WaveArchiveFileReader(const void* pWaveArchiveFile, bool isIndividual);
+    
+    void Initialize(const void* pWaveArchiveFile, bool isIndividual);
+
+    bool HasIndividualLoadTable() const;
+
+    void Finalize();
+
+    void InitializeFileTable();
+
+    u32 GetWaveFileCount() const;
+    void* GetWaveFile(u32 waveIndex) const;
+    u32 GetWaveFileSize(u32 waveIndex) const;
+    u32 GetWaveFileOffsetFromFileHead(u32 waveIndex) const;
+
+    void* SetWaveFile(u32 waveIndex, const void* pWaveFile);
+
+private:
+    WaveArchiveFile::FileHeader* m_pHeader;
+    WaveArchiveFile::InfoBlockBody* m_pInfoBlockBody;
+    IndividualLoadTable* m_pLoadTable;
+    bool m_IsInitialized;
+};
+static_assert(sizeof(WaveArchiveFileReader) == 0x20);
+} // namespace nn::atk::detail
