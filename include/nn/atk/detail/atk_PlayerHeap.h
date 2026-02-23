20bc3011318c571e840229bfd6065751b508a5f9
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-23 17:59:27 -0300

    atk: Define `detail::PlayerHeap` and `detail::CallbackNode`

---
+++ include/nn/atk/detail/atk_PlayerHeap.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <nn/util/util_IntrusiveList.h>
+
+#include <nn/atk/atk_SoundMemoryAllocatable.h>
+
+namespace nn::atk::detail {
+class PlayerHeap;
+class SoundPlayer;
+
+class CallbackNode {
+private:
+    friend PlayerHeap;
+
+    util::IntrusiveListNode m_Link;
+    SoundMemoryAllocatable::DisposeCallback m_Callback;
+    void* m_CallbackArg;
+};
+static_assert(sizeof(CallbackNode) == 0x20);
+
+class PlayerHeap : SoundMemoryAllocatable {
+public:
+    using CallbackList = util::IntrusiveList<
+                            CallbackNode, util::IntrusiveListMemberNodeTraits<
+                                CallbackNode, &CallbackNode::m_Link>>;
+
+    PlayerHeap();
+    ~PlayerHeap() override;
+
+    bool Create(void* startAddress, std::size_t size);
+    void Clear();
+
+    void* Allocate(std::size_t size) override;
+    void* Allocate(std::size_t size, 
+                   SoundMemoryAllocatable::DisposeCallback callback, void* callbackArg) override;
+
+    std::size_t GetAllocateSize(std::size_t size, bool needMemoryPool) override;
+    std::size_t GetFreeSize() const;
+
+private:
+    SoundPlayer* m_pPlayer;
+    void* m_pStartAddress;
+    void* m_pEndAddress;
+    void* m_pAllocAddress;
+    u8 m_State;
+    u8 m_Padding[3];
+    util::IntrusiveListNode m_Link;
+    CallbackList m_CallbackList;
+};
+static_assert(sizeof(PlayerHeap) == 0x50);
+} // namespace nn::atk::detail
