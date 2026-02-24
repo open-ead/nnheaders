f46d64fd87f01cde56d5726b093c25a758100c92
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-24 10:17:35 -0300

    audio: Define AudioRenderer types

--- include/nn/audio/audio_Common.h
+++ include/nn/audio/audio_Common.h
@@ -4,4 +4,13 @@
 
 namespace nn::audio {
 using NodeId = uint32_t;
+
+class VoiceInfoManager {};
+class MixManager {};
+class EffectManager {};
+class SinkManager {};
+class PerformanceBufferManager {};
+class MemoryPoolManager {};
+class BehaviorManager {};
+class SplitterInfoManager {};
 } // namespace nn::audio
