873b0e11bded70735a4514e734d4ecf767986f91
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-25 13:08:27 -0300

    atk: Define `detail::AdvancedWaveSoundFileReader`
    Other changes:
    - Defined other structs accompanying `AdvancedWaveSoundFileReader`
    
    - Declared methods for structs in `AdvancedWaveSoundFile`

--- include/nn/atk/detail/atk_AdvancedWaveSoundFile.h
+++ include/nn/atk/detail/atk_AdvancedWaveSoundFile.h
@@ -8,18 +8,31 @@ namespace nn::atk::detail {
 struct AdvancedWaveSoundFile {
     util::BinaryFileHeader fileHeader;
     
+    struct WaveSoundTrack;
     struct InfoBlockBody {
         u32 offsetToTrackTableReference;
         BinaryTypes::Reference toTrackTable;
+
+        BinaryTypes::ReferenceTable* GetTrackReferenceTable() const;
+        WaveSoundTrack* GetWaveSoundTrack(s32 index) const;
     };
     static_assert(sizeof(InfoBlockBody) == 0x8);
 
     struct InfoBlock {
         util::BinaryBlockHeader blockHeader;
         InfoBlockBody body;
-
     };
     static_assert(sizeof(InfoBlock) == 0x18);
+
+    struct WaveSoundClip;
+    struct WaveSoundTrack {
+        u32 offsetToCurveTableReference;
+        u32 offsetToClipTableReference;
+        BinaryTypes::Reference toClipTable;
+
+        BinaryTypes::ReferenceTable* GetClipReferenceTable() const;
+        WaveSoundClip* GetWaveSoundClip(s32 index) const;
+    };
 
     struct WaveSoundClip {
         u32 waveIndex;
@@ -33,11 +46,7 @@ struct AdvancedWaveSoundFile {
     };
     static_assert(sizeof(WaveSoundClip) == 0x18);
 
-    struct WaveSoundTrack {
-        u32 offsetToCurveTableReference;
-        u32 offsetToClipTableReference;
-        BinaryTypes::Reference toClipTable;
-    };
+    InfoBlock* GetBlock() const;
 };
 static_assert(sizeof(AdvancedWaveSoundFile) == 0x20);
 
