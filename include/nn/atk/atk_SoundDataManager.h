2237591a8e41a9e0b906d54be5ef5644f260dda9
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 15:38:28 -0300

    atk: Define `SoundDataManager`

--- include/nn/atk/atk_SoundDataManager.h
+++ include/nn/atk/atk_SoundDataManager.h
@@ -1,25 +1,58 @@
-/**
- * @file SoundDataManager.h
- * @brief Sound data management implementation.
- */
-
 #pragma once
 
 #include <nn/types.h>
 
-namespace nn {
-namespace atk {
-class SoundDataManager {
+#include <nn/atk/detail/atk_DisposeCallback.h>
+#include <nn/atk/detail/atk_SoundArchiveLoader.h>
+#include <nn/atk/detail/util/atk_Util.h>
+
+namespace nn::atk {
+
+namespace detail {
+class SoundFileManager;
+} // namespace nn::atk::detail
+
+class SoundDataManager : detail::driver::DisposeCallback, detail::SoundArchiveLoader {
 public:
+    struct FileAddress {
+        void* address;
+    };
+
+    using FileTable = detail::Util::Table<FileAddress>;
+
+    constexpr static u32 BufferAlignSize = 8;
+
     SoundDataManager();
-    virtual ~SoundDataManager();
+    ~SoundDataManager() override;
 
-    virtual void InvalidateData(void const*, void const*);
-    virtual void SetFileAddressToTable(u32, void const*);
-    virtual u64 GetFileAddressFromTable(u32) const;
-    virtual u32 GetFileAddressImpl(u32) const;
+    std::size_t GetRequiredMemSize(const SoundArchive* arc);
 
-    u8 _0[0x240];
+    bool Initialize(const SoundArchive* pArchive, void* buffer, std::size_t size);
+    
+    bool CreateTables(void**, const SoundArchive* pArchive, void*);
+    
+    void Finalize();
+
+    void InvalidateData(const void* start, const void* end) override;
+
+    void* detail_GetFileAddress(SoundArchive::FileId fileId) const;
+    void* GetFileAddressImpl(SoundArchive::FileId fileId) const override;
+
+    void* SetFileAddressToTable(SoundArchive::FileId fileId, const void* address) override;
+    void* GetFileAddressFromTable(SoundArchive::FileId fileId) const override;
+
+    void* SetFileAddress(SoundArchive::FileId fileId, const void* address);
+
+    SoundArchive::FileId detail_GetFileIdFromTable(const void*) const;
+
+    bool SetFileAddressInGroupFile(const void*, std::size_t);
+    void ClearFileAddressInGroupFile(const void*, std::size_t);
+
+    void InvalidateSoundData(const void*, std::size_t);
+
+private:
+    FileTable* m_pFileTable;
+    detail::SoundFileManager* m_pFileManager;
 };
-}  // namespace atk
-}  // namespace nn
+static_assert(sizeof(SoundDataManager) == 0x240);
+}  // namespace nn::atk
