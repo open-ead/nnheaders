9e3426bb258b07bde37bb8091db5731e1eebf7c8
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 19:15:06 -0300

    atk: Define `detail::driver::DisposeCallbackManager`

--- include/nn/atk/detail/atk_DisposeCallback.h
+++ include/nn/atk/detail/atk_DisposeCallback.h
@@ -10,6 +10,8 @@ public:
     virtual void InvalidateData(const void* start, const void* end) = 0;
     
 private:
+    friend class DisposeCallbackManager;
+
     util::IntrusiveListNode m_DisposeLink;
 };
 static_assert(sizeof(DisposeCallback) == 0x18);
