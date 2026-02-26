2ee338c1061117e6c838f7c91aadec2157977ac1
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 15:17:19 -0300

    atk: Define `detail::AdvancedWaveSound`

---
+++ include/nn/atk/detail/atk_AdvancedWaveSound.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <nn/atk/detail/atk_BasicSound.h>
+#include <nn/atk/detail/atk_SoundInstanceManager.h>
+#include <nn/atk/detail/atk_AdvancedWaveSoundPlayer.h>
+
+namespace nn::atk::detail {
+class AdvancedWaveSound;
+class AdvancedWaveSoundHandle;
+using AdvancedWaveSoundInstanceManager = SoundInstanceManager<AdvancedWaveSound>;
+
+class AdvancedWaveSound : BasicSound {
+public:
+    explicit AdvancedWaveSound(const AdvancedWaveSoundInstanceManager& manager);
+    ~AdvancedWaveSound() override;
+
+    bool Initialize(OutputReceiver* pOutputReceiver) override;
+    void Finalize() override;
+
+    void Prepare(const driver::AdvancedWaveSoundPlayer::PrepareParameter& parameter);
+
+    void OnUpdatePlayerPriority() override;
+    
+    bool IsAttachedTempSpecialHandle() override;
+    void DetachTempSpecialHandle() override;
+
+    bool IsPrepared();
+
+    driver::BasicSoundPlayer* GetBasicSoundPlayerHandle();
+
+private:
+    util::IntrusiveListNode m_PriorityLink;
+    AdvancedWaveSoundHandle* m_pTempSpecialHandle;
+    AdvancedWaveSoundInstanceManager* m_InstanceManager;
+    driver::AdvancedWaveSoundPlayer m_PlayerInstance;
+    bool m_IsInitialized;
+    u8 m_Padding[3];
+};
+static_assert(sizeof(AdvancedWaveSound) == 0x9b0);
+} // namespace nn::atk::detail
