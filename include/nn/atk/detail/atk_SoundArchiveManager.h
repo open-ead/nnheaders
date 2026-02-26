0939c4dac0b23e685acf3dd579bc7174fbef0a7b
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 20:10:00 -0300

    atk: Redefine `detail::SoundArchiveManager`

--- include/nn/atk/detail/atk_SoundArchiveManager.h
+++ include/nn/atk/detail/atk_SoundArchiveManager.h
@@ -1,42 +1,52 @@
-/**
- * @file SoundArchiveManager.h
- * @brief Sound archive manager implementation.
- */
-
 #pragma once
 
-#include <nn/types.h>
+#include <nn/atk/atk_SoundArchive.h>
+#include <nn/atk/atk_SoundDataManager.h>
+#include <nn/atk/detail/atk_AddonSoundArchiveContainer.h>
+#include <nn/atk/detail/atk_IntrusiveList.h>
 
-namespace nn {
-namespace atk {
-class SoundHandle;
-class SoundArchive;
-class SoundDataManager;
-
-namespace detail {
-class AddonSoundArchiveContainer;
-
+namespace nn::atk::detail {
 class SoundArchiveManager {
 public:
+    using ContainerList = IntrusiveList<AddonSoundArchiveContainer>;
+
+    class Snapshot {
+    public:
+    private:
+        SoundArchive* m_MainSoundArchive;
+        SoundDataManager* m_MainSoundDataManager;
+        SoundArchive* m_CurrentSoundArchive;
+        SoundDataManager* m_CurrentSoundDataManager;
+    };
+
     SoundArchiveManager();
+    ~SoundArchiveManager();
 
-    virtual ~SoundArchiveManager();
+    void Initialize(const SoundArchive* pSoundArchive, const SoundDataManager* pSoundDataManager);
 
-    void Initialize(nn::atk::SoundArchive const*, nn::atk::SoundDataManager const*);
-    void ChangeTargetArchive(char const*);
+    void ChangeTargetArchive(const char* soundArchiveName);
+
     void Finalize();
-    bool IsAvailable() const;
-    nn::atk::detail::AddonSoundArchiveContainer* GetAddonSoundArchive(char const*) const;
 
-    u64 _8;
-    u64* _10;
-    nn::atk::detail::AddonSoundArchiveContainer* _18;
-    u64* _20;
-    nn::atk::SoundArchive* mSoundArchive;  // _28
-    u64 _30;
-    u64 _38;
-    u64 _40;
+    void Add(AddonSoundArchiveContainer&);
+    void Remove(AddonSoundArchiveContainer&);
+
+    bool IsAvailable() const;
+
+    AddonSoundArchive* GetAddonSoundArchive(const char*) const;
+    SoundDataManager* GetAddonSoundDataManager(const char*) const;
+    AddonSoundArchiveContainer* GetAddonSoundArchiveContainer(s32) const;
+    AddonSoundArchiveContainer* GetAddonSoundArchiveContainer(s32);
+
+    void SetParametersHook(SoundArchiveParametersHook*);
+
+private:
+    SoundArchive* m_pMainSoundArchive;
+    SoundDataManager* m_pMainSoundDataManager;
+    ContainerList m_ContainerList;
+    SoundArchive* m_pCurrentSoundArchive;
+    SoundDataManager* m_pCurrentSoundDataManager;
+    SoundArchiveParametersHook* m_pParametersHook;
 };
-}  // namespace detail
-}  // namespace atk
-}  // namespace nn
+static_assert(sizeof(SoundArchiveManager) == 0x38);
+}  // namespace nn::atk::detail
