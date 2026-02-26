a0b2a55f96e6b4462fdf643fee1172f58c2af389
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 16:38:45 -0300

    atk: Define `detail::driver::StreamBufferPool`

---
+++ include/nn/atk/detail/strm/atk_StreamBufferPool.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <nn/types.h>
+
+namespace nn::atk::detail::driver {
+class StreamBufferPool {
+public:
+    constexpr static u32 BlockMax = 32;
+    constexpr static u8 BitPerByte = 8;
+
+    void Initialize(void* buffer, std::size_t size, s32 blockCount);
+    void Finalize();
+
+    void* Alloc();
+    void Free(void* pPtr);
+
+private:
+    void* m_Buffer;
+    std::size_t m_BufferSize;
+    std::size_t m_BlockSize;
+    s32 m_BlockCount;
+    s32 m_AllocCount;
+    u8 m_AllocFlags[4];
+};
+static_assert(sizeof(StreamBufferPool) == 0x28);
+} // namespace nn::atk::detail
