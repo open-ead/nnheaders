e46546713bd635c9db23f3feea8db923c5356ae3
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 19:15:06 -0300

    atk: Define `detail::driver::DisposeCallbackManager`

---
+++ include/nn/atk/detail/atk_DisposeCallbackManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <nn/types.h>
+
+#include <nn/atk/detail/atk_DisposeCallback.h>
+
+namespace nn::atk::detail::driver {
+class DisposeCallbackManager {
+public:
+    using CallbackList = util::IntrusiveList<DisposeCallback, 
+                            util::IntrusiveListMemberNodeTraits<DisposeCallback, 
+                                &DisposeCallback::m_DisposeLink>>;
+
+    DisposeCallbackManager();
+
+    void RegisterDisposeCallback(DisposeCallback* callback);
+    void UnregisterDisposeCallback(DisposeCallback* callback);
+
+    u64 GetCallbackCount() const;
+
+    void Dispose(const void* mem, std::size_t size);
+
+private:
+    CallbackList m_CallbackList;
+};
+static_assert(sizeof(DisposeCallbackManager) == 0x10);
+} // namespace nn::atk::detail
