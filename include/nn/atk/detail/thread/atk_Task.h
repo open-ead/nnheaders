6b62b4c046fba793e4bd3c44e6678324a3c2b0f4
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-26 23:19:30 -0300

    atk: Define `detail::Task`

---
+++ include/nn/atk/detail/thread/atk_Task.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <nn/atk/util/atk_TaskProfileReader.h>
+
+namespace nn::atk::detail {
+class Task {
+public:
+    enum Status {
+        Status_Free,
+        Status_Append,
+        Status_Execute,
+        Status_Done,
+        Status_Cancel,
+    };
+
+    Task();
+    
+    virtual ~Task();
+    virtual void Execute(TaskProfileLogger& logger) = 0;
+
+private:
+    util::IntrusiveListNode m_TaskLink;
+    os::Event m_Event;
+    Status m_Status;
+    u32 m_Id;
+};
+static_assert(sizeof(Task) == 0x48);
+} // namespace nn::atk::detail
