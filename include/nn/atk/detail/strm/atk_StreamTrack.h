b1b9e8a38d47dfd386456f5d4e19d48a359a7f7f
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-27 12:24:54 -0300

    atk: Define `detail::driver::StreamTrack`

--- include/nn/atk/detail/strm/atk_StreamTrack.h
+++ include/nn/atk/detail/strm/atk_StreamTrack.h
@@ -15,4 +15,28 @@ private:
     UpdateType m_UpdateType;
 };
 static_assert(sizeof(StreamChannel) == 0x1080);
+
+class StreamTrack {
+public:
+    StreamTrack();
+    ~StreamTrack();
+    
+private:
+    bool m_ActiveFlag;
+    StreamChannel* m_pChannels[2];
+    u8 channelCount;
+    u8 volume;
+    u8 pan;
+    u8 span;
+    u8 mainSend;
+    u8 fxSend[3];
+    u8 lpfFreq;
+    s8 biquadType;
+    u8 biquadValue;
+    u8 flags;
+    f32 m_Volume;
+    s32 m_OutputLine;
+    OutputParam m_TvParam;
+};
+static_assert(sizeof(StreamTrack) == 0x80);
 } // namespace nn::atk::detail::driver
