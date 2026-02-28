dcd92bebbcdfc244b1d9a7732267016918889dec
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-28 12:19:27 -0300

    atk: Redefine `detail::WaveSoundRuntime`

--- include/nn/atk/detail/wsd/atk_WaveSoundRuntime.h
+++ include/nn/atk/detail/wsd/atk_WaveSoundRuntime.h
@@ -1,29 +1,45 @@
-/**
- * @file WaveSoundRuntime.h
- * @brief Wave sound runtime info.
- */
-
 #pragma once
 
-#include <nn/types.h>
+#include <nn/atk/detail/atk_StartInfoReader.h>
+#include <nn/atk/detail/wsd/atk_WaveSound.h>
 
-namespace nn {
-namespace atk {
-namespace detail {
+namespace nn::atk::detail {
 class WaveSoundRuntime {
 public:
     WaveSoundRuntime();
     ~WaveSoundRuntime();
 
-    void Initialize(s32, void**, void const*);
+    void Initialize(s32 soundCount, void** pOutAllocatedAddr, const void* endAddr);
     void Finalize();
+
+    static std::size_t GetRequiredMemorySize(const SoundArchive::SoundArchivePlayerInfo& soundArchivePlayerInfo, 
+                                             s32 alignment);
+
     s32 GetActiveCount() const;
     s32 GetFreeWaveSoundCount() const;
-    void SetupUserParam(void**, u64);
-    void Update();
 
-    u8 _0[0x80];
+    void SetupUserParam(void** startAddr, std::size_t adjustSize);
+
+    void Update();
+    
+    WaveSound* AllocSound(SoundArchive::ItemId soundId, s32 priority, s32 ambientPriority, 
+                          BasicSound::AmbientInfo* ambientArgInfo);
+    WaveSound* AllocSound(SoundArchive::ItemId soundId, s32 priority, s32 ambientPriority, 
+                          BasicSound::AmbientInfo* ambientArgInfo, OutputReceiver* pOutputReceiver);
+
+    SoundStartable::StartResult PrepareImpl(SoundArchive* pSoundArchive, 
+                                            SoundDataManager* pSoundDataManager, 
+                                            SoundArchive::ItemId soundId, 
+                                            WaveSound* sound, 
+                                            SoundArchive::SoundInfo* commonInfo, 
+                                            StartInfoReader* startInfoReader);
+
+    void DumpMemory(const SoundArchive*);
+
+private:
+    WaveSoundInstanceManager m_WaveSoundInstanceManager;
+    driver::WaveSoundLoaderManager m_WaveSoundLoaderManager;
+    SoundArchiveFilesHook* m_pSoundArchiveFilesHook;
 };
-}  // namespace detail
-}  // namespace atk
-}  // namespace nn
+static_assert(sizeof(WaveSoundRuntime) == 0x88);
+}  // namespace nn::atk::detail
