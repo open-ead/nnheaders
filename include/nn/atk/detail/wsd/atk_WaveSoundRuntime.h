15f0aa7cb5cca79320a3355c8e053704c628029c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-02-28 12:53:18 -0300

    atk: Redefine `detail::StreamSoundRuntime`

--- include/nn/atk/detail/wsd/atk_WaveSoundRuntime.h
+++ include/nn/atk/detail/wsd/atk_WaveSoundRuntime.h
@@ -9,7 +9,7 @@ public:
     WaveSoundRuntime();
     ~WaveSoundRuntime();
 
-    void Initialize(s32 soundCount, void** pOutAllocatedAddr, const void* endAddr);
+    bool Initialize(s32 soundCount, void** pOutAllocatedAddr, const void* endAddr);
     void Finalize();
 
     static std::size_t GetRequiredMemorySize(const SoundArchive::SoundArchivePlayerInfo& soundArchivePlayerInfo, 
@@ -27,12 +27,12 @@ public:
     WaveSound* AllocSound(SoundArchive::ItemId soundId, s32 priority, s32 ambientPriority, 
                           BasicSound::AmbientInfo* ambientArgInfo, OutputReceiver* pOutputReceiver);
 
-    SoundStartable::StartResult PrepareImpl(SoundArchive* pSoundArchive, 
-                                            SoundDataManager* pSoundDataManager, 
+    SoundStartable::StartResult PrepareImpl(const SoundArchive* pSoundArchive, 
+                                            const SoundDataManager* pSoundDataManager, 
                                             SoundArchive::ItemId soundId, 
                                             WaveSound* sound, 
-                                            SoundArchive::SoundInfo* commonInfo, 
-                                            StartInfoReader* startInfoReader);
+                                            const SoundArchive::SoundInfo* commonInfo, 
+                                            const StartInfoReader& startInfoReader);
 
     void DumpMemory(const SoundArchive*);
 
