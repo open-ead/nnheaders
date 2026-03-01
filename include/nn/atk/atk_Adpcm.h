4622b766094845b7ad2023b70056e87e911ab868
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-03-01 17:49:24 -0300

    atk: Define global functions and variables

--- include/nn/atk/atk_Adpcm.h
+++ include/nn/atk/atk_Adpcm.h
@@ -2,6 +2,8 @@
 
 #include <nn/audio/audio_Adpcm.h>
 
+#include <nn/atk/detail/atk_Config.h>
+
 namespace nn::atk {
 using AdpcmParam = audio::AdpcmParameter;
 
@@ -14,4 +16,10 @@ struct AdpcmContextNotAligned {
     audio::AdpcmContext audioAdpcmContext;
 };
 static_assert(sizeof(AdpcmContextNotAligned) == 0x6);
+
+namespace detail {
+void DecodeDspAdpcm(position_t playPosition, AdpcmContext* context, 
+                    AdpcmParam* param, void* adpcmData, size_t decodeSamples,
+                    s16* dest);
+};
 } // namespace nn::atk
