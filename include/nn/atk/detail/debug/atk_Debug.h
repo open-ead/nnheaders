44976b1c9eeeb73b6b61f22e53e9e488a4d99d7b
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-03-01 17:49:24 -0300

    atk: Define global functions and variables

--- include/nn/atk/detail/debug/atk_Debug.h
+++ include/nn/atk/detail/debug/atk_Debug.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <nn/types.h>
+
 namespace nn::atk {
 enum DebugWarningFlag {
     DebugWarningFlag_NotEnoughInstance,
@@ -10,6 +12,11 @@ enum DebugWarningFlag {
     DebugWarningFlag_NotEnoughStrmchannel,
 };
 
+static u8 gWarningFlag = 0b11111;
+static const u32 gWarningBitFlags[6] = {0b11111, 0b00001, 0b00010, 0b00100, 0b01000, 0b10000};
+
+void Debug_SetWarningFlag(DebugWarningFlag warning, bool);
+
 namespace detail {
 enum DebugSoundType {
     DebugSoundType_Seqsound,
@@ -17,6 +24,14 @@ enum DebugSoundType {
     DebugSoundType_Wavesound,
 };
 
+static const char* gSoundTypeStrings[3] = {"seq", "strm", "wave"};
+
+bool Debug_GetWarningFlag(DebugWarningFlag warning);
+DebugWarningFlag Debug_GetDebugWarningFlagFromSoundType(DebugSoundType type);
+char* Debug_GetSoundTypeString(DebugSoundType type);
+
 using DebugLogFunc = void(*)(char*);
+
+static DebugLogFunc g_DebugLogHookFunc;
 } // namespace nn::atk::detail
 } // namespace nn::atk
