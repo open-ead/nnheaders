084cc4b1dbd4cb4a675663dd6ee9f51390b90ad9
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2026-07-19 11:41:31 +0200

    add:partial err implementation

--- include/nn/fs/fs_mount.h
+++ include/nn/fs/fs_mount.h
@@ -2,6 +2,10 @@
 
 #include <nn/fs/fs_types.h>
 
+namespace nn::ncm {
+enum class SystemDataId { Err = 0x801 };
+}  // namespace nn::ncm
+
 namespace nn::fs {
 
 /*
@@ -11,6 +15,7 @@ namespace nn::fs {
 bool MountSdCardForDebug(const char* mount);
 
 Result MountSdCard(const char* mountPoint);
+Result MountSystemData(const char* mountPoint, ncm::SystemDataId id);
 bool IsSdCardInserted();
 Result FormatSdCard();
 Result FormatSdCardDryRun();
