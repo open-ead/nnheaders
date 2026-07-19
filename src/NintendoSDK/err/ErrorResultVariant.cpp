385c2429b1e778c56c5571c039409fa1aac18ca8
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2026-07-19 22:27:06 +0200

    add:partial err implementation

---
+++ src/NintendoSDK/err/ErrorResultVariant.cpp
@@ -0,0 +1,33 @@
+#include <nn/err/ErrorResultVariant.h>
+
+namespace nn::err {
+ErrorResultVariant::ErrorResultVariant() {}
+
+ErrorResultVariant::ErrorResultVariant(const Result& result)
+    : mState(State::Result), mValue({.mResult = result}) {}
+
+ErrorResultVariant::ErrorResultVariant(const ErrorCode& errorCode)
+    : mState(State::ErrorCode), mValue({.mErrorCode = errorCode}) {}
+
+void ErrorResultVariant::operator=(const Result& result) {
+    mState = State::Result;
+    mValue.mResult = result;
+}
+
+void ErrorResultVariant::operator=(const ErrorCode& errorCode) {
+    mState = State::ErrorCode;
+    mValue.mErrorCode = errorCode;
+}
+
+ErrorResultVariant::operator Result() const {
+    return mValue.mResult;
+}
+
+ErrorResultVariant::operator ErrorCode() const {
+    return mValue.mErrorCode;
+}
+
+ErrorResultVariant::State ErrorResultVariant::GetState() const {
+    return mState;
+}
+}  // namespace nn::err
