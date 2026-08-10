03407e375c2f906c6c2451c26b8b40b466230771
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-10 19:19:29 -0400

    atk: Match `StreamSoundFile::FileHeader::GetRegionBlockSize`

--- src/NintendoWare/atk/detail/strm/atk_StreamSoundFile.cpp
+++ src/NintendoWare/atk/detail/strm/atk_StreamSoundFile.cpp
@@ -23,6 +23,10 @@ u32 StreamSoundFile::FileHeader::GetDataBlockSize() const {
     return GetReferenceBy(ElementType_StreamSoundFile_DataBlock)->size;
 }
 
+u32 StreamSoundFile::FileHeader::GetRegionBlockSize() const {
+    return GetReferenceBy(ElementType_StreamSoundFile_RegionBlock)->size;
+}
+
 const Util::ReferenceWithSize* StreamSoundFile::FileHeader::GetReferenceBy(u16 typeId) const {
     for (int i {0}; i < dataBlocks; ++i) {
         if (toBlocks[i].typeId == typeId)
