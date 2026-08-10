ccc97230888cf51708273fff44b965b302db8d90
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-10 18:05:43 -0400

    atk: Match `StreamSoundFile::FileHeader::HasRegionBlock`

--- src/NintendoWare/atk/detail/strm/atk_StreamSoundFile.cpp
+++ src/NintendoWare/atk/detail/strm/atk_StreamSoundFile.cpp
@@ -7,6 +7,10 @@ bool StreamSoundFile::FileHeader::HasSeekBlock() const {
     return GetReferenceBy(ElementType_StreamSoundFile_SeekBlock) != nullptr;
 }
 
+bool StreamSoundFile::FileHeader::HasRegionBlock() const {
+    return GetReferenceBy(ElementType_StreamSoundFile_RegionBlock) != nullptr;
+}
+
 const Util::ReferenceWithSize* StreamSoundFile::FileHeader::GetReferenceBy(u16 typeId) const {
     for (int i {0}; i < dataBlocks; ++i) {
         if (toBlocks[i].typeId == typeId)
