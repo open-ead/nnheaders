ac185a993d820a323748d49a4ec8dd2008537f7f
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-15 16:04:21 -0400

    atk: Match `WaveFile::FileHeader::GetDataBlock`

--- src/NintendoWare/atk/detail/atk_WaveFile.cpp
+++ src/NintendoWare/atk/detail/atk_WaveFile.cpp
@@ -7,4 +7,9 @@ const WaveFile::InfoBlock* WaveFile::FileHeader::GetInfoBlock() const {
     return util::ConstBytePtr(GetBlock(ElementType_WaveFile_InfoBlock))
         .Get<WaveFile::InfoBlock>();
 }
+
+const WaveFile::DataBlock* WaveFile::FileHeader::GetDataBlock() const {
+    return util::ConstBytePtr(GetBlock(ElementType_WaveFile_DataBlock))
+        .Get<WaveFile::DataBlock>();
+}
 } // namespace nn::atk::detail
