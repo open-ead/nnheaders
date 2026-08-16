233e3caf280ef2cd66d44cf04d4bfd633f315a4c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-16 11:48:18 -0400

    atk: Match `AudioRendererPerformanceReader::GetRequiredMemorySize`

--- src/NintendoWare/atk/util/atk_AudioRendererPerformanceReader.cpp
+++ src/NintendoWare/atk/util/atk_AudioRendererPerformanceReader.cpp
@@ -3,4 +3,10 @@
 
 namespace nn::atk {
 AudioRendererPerformanceReader::AudioRendererPerformanceReader() = default;
+
+size_t AudioRendererPerformanceReader::GetRequiredMemorySize(int performanceInfoCount) {
+    size_t size {performanceInfoCount * 
+                (SoundSystem::GetPerformanceFrameBufferSize() + sizeof(PerformanceInfo))};
+    return size;
+}
 } // namespace nn::atk
