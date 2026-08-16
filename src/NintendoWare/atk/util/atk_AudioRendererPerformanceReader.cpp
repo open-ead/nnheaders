1f943633d71a83dcc439f0bf5fa840b9d433cab6
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-16 11:11:52 -0400

    atk: Match `AudioRendererPerformanceReader` constructor

---
+++ src/NintendoWare/atk/util/atk_AudioRendererPerformanceReader.cpp
@@ -0,0 +1,6 @@
+#include <nn/atk/atk_AudioRendererPerformanceReader.h>
+#include <nn/atk/atk_SoundSystem.h>
+
+namespace nn::atk {
+AudioRendererPerformanceReader::AudioRendererPerformanceReader() = default;
+} // namespace nn::atk
