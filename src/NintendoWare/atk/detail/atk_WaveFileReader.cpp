785f9e5cca3ed8e19852d131f39a6bddfee09435
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-16 11:00:30 -0400

    atk: Match `WaveFileReader::ReadWaveInfo`
    This matches all exported functions in `WaveFileReader`

--- src/NintendoWare/atk/detail/atk_WaveFileReader.cpp
+++ src/NintendoWare/atk/detail/atk_WaveFileReader.cpp
@@ -65,6 +65,56 @@ bool WaveFileReader::IsOriginalLoopAvailable() const {
     return header.version >= IncludeOriginalLoopVersionWav;
 }
 
+bool WaveFileReader::ReadWaveInfo(WaveInfo* info, const void* waveDataOffsetOrigin) const {
+    switch (m_WaveType) {
+    case WaveType_Nwwav: {
+        const SampleFormat format {GetSampleFormat(m_pInfoBlockBody->encoding)};
+        const int channelCount {m_pInfoBlockBody->GetChannelCount()};
+
+        info->sampleFormat = format;
+        info->channelCount = channelCount;
+        info->sampleRate = m_pInfoBlockBody->sampleRate;
+        info->loopFlag = m_pInfoBlockBody->isLoop == 1;
+        info->loopStartFrame = m_pInfoBlockBody->loopStartFrame;
+        info->loopEndFrame = m_pInfoBlockBody->loopEndFrame;
+        info->dataSize = m_pHeader->header.fileSize - sizeof(Util::SoundFileHeader);
+
+        if (IsOriginalLoopAvailable()) 
+            info->originalLoopStartFrame = m_pInfoBlockBody->originalLoopStartFrame;
+        else
+            info->originalLoopStartFrame = m_pInfoBlockBody->loopStartFrame;
+
+        for (int i {0}; i < channelCount; ++i) {
+            if (i < 2) {
+                WaveInfo::ChannelParam& channelParam {info->channelParam[i]};
+                const WaveFile::ChannelInfo& channelInfo {m_pInfoBlockBody->GetChannelInfo(i)};
+                
+                if (channelInfo.referToAdpcmInfo.offset != 0) {
+                    const WaveFile::DspAdpcmInfo& adpcmInfo {channelInfo.GetDspAdpcmInfo()};
+                    channelParam.adpcmParam = adpcmInfo.adpcmParam;
+                    channelParam.adpcmLoopParam = adpcmInfo.adpcmLoopParam;
+                }
+    
+                channelParam.dataAddress = GetWaveDataAddress(&channelInfo, waveDataOffsetOrigin);
+                
+                const WaveFile::ChannelInfo& lastChannelInfo {m_pInfoBlockBody->GetChannelInfo(channelCount - 1)};
+                channelParam.dataSize = m_pHeader->GetDataBlock()->header.size 
+                                        - sizeof(BinaryBlockHeader)
+                                        - lastChannelInfo.referToSamples.offset;
+            }
+        }
+        break;
+    }
+    case WaveType_Dspadpcm:
+        m_DspadpcmReader.ReadWaveInfo(info);
+        break;
+    case WaveType_Invalid:
+        break;
+    }
+
+    return true;
+}
+
 const void* WaveFileReader::GetWaveDataAddress(const WaveFile::ChannelInfo* info, const void* waveDataOffsetOrigin) const {
     return info->GetSamplesAddress(m_pDataBlockBody);
 }
