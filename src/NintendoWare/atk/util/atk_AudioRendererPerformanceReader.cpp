a134268802c94da2ffad53d459d9c0b60d8d2353
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-16 19:26:14 -0400

    atk: Match `AudioRendererPerformanceReader::Initialize`

--- src/NintendoWare/atk/util/atk_AudioRendererPerformanceReader.cpp
+++ src/NintendoWare/atk/util/atk_AudioRendererPerformanceReader.cpp
@@ -9,4 +9,24 @@ size_t AudioRendererPerformanceReader::GetRequiredMemorySize(int performanceInfo
                 (SoundSystem::GetPerformanceFrameBufferSize() + sizeof(PerformanceInfo))};
     return size;
 }
+
+void AudioRendererPerformanceReader::Initialize(int performanceInfoCount, void* buffer, size_t bufferSize) {
+    m_PerformanceInfoCount = performanceInfoCount;
+    
+    auto ptr {util::BytePtr(buffer)};
+    m_pPerformanceInfo = ptr.Get<PerformanceInfo>();
+
+    ptr += performanceInfoCount * sizeof(PerformanceInfo);
+    
+    const size_t performanceBufferSize {SoundSystem::GetPerformanceFrameBufferSize()};
+    for (int i {0}; i < m_PerformanceInfoCount; ++i) {
+        m_pPerformanceInfo[i].performanceBuffer = ptr.Get();
+        m_pPerformanceInfo[i].performanceBufferSize = performanceBufferSize;
+        ptr += performanceBufferSize;
+    }
+
+    m_WriteIndex = 0;
+    m_ReadIndex = m_PerformanceInfoCount - 1;
+    m_IsInitialized = true;
+}
 } // namespace nn::atk
