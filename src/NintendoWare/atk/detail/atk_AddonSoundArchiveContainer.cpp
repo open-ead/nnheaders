c36c8adf988302f67d248b067d6fbabaacdfe5b5
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-18 18:07:22 -0400

    atk: Match `AddonSoundArchiveContainer` destructor

--- src/NintendoWare/atk/detail/atk_AddonSoundArchiveContainer.cpp
+++ src/NintendoWare/atk/detail/atk_AddonSoundArchiveContainer.cpp
@@ -3,6 +3,12 @@
 namespace nn::atk::detail {
 AddonSoundArchiveContainer::AddonSoundArchiveContainer() = default;
 
+AddonSoundArchiveContainer::~AddonSoundArchiveContainer() {
+    m_IsActive = false;
+    m_pSoundArchive = nullptr;
+    m_pSoundDataManager = nullptr;
+}
+
 void AddonSoundArchiveContainer::Finalize() {
     m_IsActive = false;
     m_SoundArchiveName[0] = '\0';
