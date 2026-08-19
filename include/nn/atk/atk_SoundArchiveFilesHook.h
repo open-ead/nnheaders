c7b3f9d1e779e50d7d1fc17e7b5e54f4e060709c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-19 11:26:15 -0400

    atk: Match `detail::DecodeDspAdpcm`
    Implementation heavily based on [TuneBloom's](https://github.com/stupidestmodder/TuneBloom/blob/master/TuneBloom/src/snd-ply/DecodeAdpcm.cpp)

--- include/nn/atk/atk_SoundArchiveFilesHook.h
+++ include/nn/atk/atk_SoundArchiveFilesHook.h
@@ -1,16 +1,35 @@
 #pragma once
 
-namespace nn::atk::detail {
-struct SoundArchiveFilesHook {
-    constexpr static const char* ItemTypeStreamSound = "stm";
-    constexpr static const char* ItemTypeWaveSound = "wsd";
-    constexpr static const char* ItemTypeSequenceSound = "seq";
+#include <nn/types.h>
+#include <nn/atk/fnd/io/atkfnd_FileStream.h>
 
-    constexpr static const char* FileTypeStreamBinary = "bxstm";
-    constexpr static const char* FileTypeWaveSoundBinary = "bxwsd";
-    constexpr static const char* FileTypeSequenceBinary = "bxseq";
-    constexpr static const char* FileTypeBankBinary = "bxbnk";
-    constexpr static const char* FileTypeWaveArchiveBinary = "bxwar";
-    constexpr static const char* FileTypeStreamPrefetchBinary = "bxstp";
+namespace nn::atk::detail {
+class SoundArchiveFilesHook {
+public:
+    constexpr static const char ItemTypeWaveSound[] = "wsd";
+    constexpr static const char ItemTypeStreamSound[] = "stm";
+    constexpr static const char ItemTypeSequenceSound[] = "seq";
+
+    constexpr static const char FileTypeStreamBinary[] = "bxstm";
+    constexpr static const char FileTypeWaveSoundBinary[] = "bxwsd";
+    constexpr static const char FileTypeSequenceBinary[] = "bxseq";
+    constexpr static const char FileTypeBankBinary[] = "bxbnk";
+    constexpr static const char FileTypeWaveArchiveBinary[] = "bxwar";
+    constexpr static const char FileTypeStreamPrefetchBinary[] = "bxstp";
+
+    bool GetIsEnable() const;
+    void SetIsEnable(bool value);
+
+    bool IsTargetItem(const char* itemLabel);
+
+    void Lock();
+    void Unlock();
+
+    fnd::FileStream* OpenFile(void* buffer, size_t bufferLength, void* cacheBuffer, size_t cacheBufferLength, const char* itemLabel, const char* fileType);
+
+    const void* GetFileAddress(const char* itemLabel, const char* itemType, const char* fileType, u32 fileIndex);
+
+private:
+    bool m_IsEnable;
 };
 } // namespace nn::atk::detail
