84d33d502d097332ae06c0f15ae0a59016334287
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-23 18:50:36 -0400

    atk: Match `BusMixVolumePacket::GetRequiredMemSize`

--- include/nn/atk/atk_BusMixVolumePacket.h
+++ include/nn/atk/atk_BusMixVolumePacket.h
@@ -3,20 +3,58 @@
 #include <nn/atk/atk_Global.h>
 
 namespace nn::atk::detail {
-class BusMixVolumePacket {
+struct BusMixVolumePacket {
 public:
-    static size_t GetRequiredMemSize(s32 busCount);
+    static size_t GetRequiredMemSize(int busCount);
 
-    bool Initialize(void* buffer, size_t size, s32 busCount);
+    BusMixVolumePacket();
+    
+    bool Initialize(void* buffer, size_t size, int busCount);
     void Finalize();
 
     void Reset();
+
+    int GetBusCount() const { 
+        return m_BusCount; 
+    }
+
+    float GetBusMixVolume(int waveChannel, int mixChannel) const {
+        return m_BusMixVolume.volume[waveChannel][mixChannel];
+    }
+
+    const OutputBusMixVolume& GetBusMixVolume() const {
+        return m_BusMixVolume;
+    }
+
+    void SetBusMixVolume(int waveChannel, int mixChannel, float volume) {
+        m_BusMixVolume.volume[waveChannel][mixChannel] = volume;
+    }
+
+    void SetBusMixVolume(const OutputBusMixVolume& busMixVolume) {
+        m_BusMixVolume = busMixVolume;
+    }
+
+    bool IsUsed() const {
+        return m_IsUsed;
+    }
+
+    void SetUsed(bool isUsed) {
+        m_IsUsed = isUsed;
+    }
+
+    bool IsEnabled(int bus) const {
+        return m_pIsEnabledTable[bus];
+    }
+
+    void SetEnabled(int bus, bool isEnabled) {
+        m_pIsEnabledTable[bus] = isEnabled;
+    }
 
 private:
     OutputBusMixVolume m_BusMixVolume;
     bool m_IsUsed;
     bool* m_pIsEnabledTable;
-    s32 m_BusCount;
+    int m_BusCount;
 };
 static_assert(sizeof(BusMixVolumePacket) == 0xd8);
 } // namespace nn::atk::detail
