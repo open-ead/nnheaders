cb78663f9736f399aa4aa098f82efd0b2d24e826
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-23 18:03:57 -0400

    atk: Add draft implementation for  `SoundArchive::detail_GetExternalFileFullPath`

--- include/nn/atk/fnd/string/atkfnd_String.h
+++ include/nn/atk/fnd/string/atkfnd_String.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <cstring>
+
+#include <nn/atk/fnd/basis/atkfnd_Config.h>
+
+namespace nn::atk::detail::fnd {
+// UNSURE
+inline size_t strncat(char* dst, size_t dstCount, const char* src, size_t srcCount) {
+    size_t length {strlen(src)};
+    if (length + srcCount + 1 < dstCount)
+        length = srcCount + 1;
+    else
+        length = dstCount - 1 - length;
+
+    std::strncat(dst, src, length);
+    return length;
+}
+} // namespace atk::detail::fnd
