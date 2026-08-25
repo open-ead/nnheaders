0b0b3e0b4c3aad7aaa4835955c202ae8e3994618
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-25 12:54:56 -0400

    atk: Match `WaveSoundFile::InfoBlockBody::GetWaveIdTable`

--- src/NintendoWare/atk/detail/wsd/atk_WaveSoundFile.cpp
+++ src/NintendoWare/atk/detail/wsd/atk_WaveSoundFile.cpp
@@ -1,10 +1,15 @@
 #include <nn/atk/atk_WaveSoundFile.h>
 
 #include <nn/atk/atk_ElementType.h>
+#include "nn/util/util_BytePtr.h"
 
 namespace nn::atk::detail {
 const WaveSoundFile::InfoBlock* WaveSoundFile::FileHeader::GetInfoBlock() const {
     return util::ConstBytePtr(GetBlock(ElementType_WaveSoundFile_InfoBlock))
             .Get<WaveSoundFile::InfoBlock>();
+}
+
+const Util::WaveIdTable& WaveSoundFile::InfoBlockBody::GetWaveIdTable() const {
+    return *util::ConstBytePtr(this, toWaveIdTable.offset).Get<Util::WaveIdTable>();
 }
 } // namespace nn::atk::detail
