935ac2ce66c77fd2f7921c960a66db94345a6883
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-25 12:52:12 -0400

    atk: Match `WaveSoundFile::FileHeader::GetInfoBlock`
    Also rearranged `WaveSoundFile` to better match DWARF

--- include/nn/atk/atk_WaveSoundFile.h
+++ include/nn/atk/atk_WaveSoundFile.h
@@ -6,21 +6,30 @@ namespace nn::atk::detail {
 struct WaveSoundFile {
     struct InfoBlock;
     struct FileHeader : Util::SoundFileHeader {
-
-        InfoBlock* GetInfoBlock() const;
-
+        const InfoBlock* GetInfoBlock() const;
     };
 
     struct WaveSoundData;
     struct InfoBlockBody {
-
-        WaveSoundData* GetWaveSoundData(u32 index) const;
-        Util::ReferenceTable* GetWaveSoundDataReferenceTable() const;
-
-        Util::WaveIdTable* GetWaveIdTable() const;
-
         Util::Reference toWaveIdTable;
         Util::Reference toWaveSoundDataReferenceTable;
+
+        const Util::WaveIdTable& GetWaveIdTable() const;
+        const Util::ReferenceTable& GetWaveSoundDataReferenceTable() const;
+        
+        u32 GetWaveIdCount() const {
+            return GetWaveIdTable().GetCount();
+        }
+
+        u32 GetWaveSoundCount() const {
+            return GetWaveSoundDataReferenceTable().count;
+        }
+
+        const Util::WaveId* GetWaveId(u32 index) const {
+            return GetWaveIdTable().GetWaveId(index);
+        }
+
+        const WaveSoundData& GetWaveSoundData(u32 index) const;
     };
     static_assert(sizeof(InfoBlockBody) == 0x10);
 
@@ -34,58 +43,72 @@ struct WaveSoundFile {
     struct TrackInfo;
     struct NoteInfo;
     struct WaveSoundData {
-        
-        WaveSoundInfo* GetWaveSoundInfo() const;
-
-        Util::ReferenceTable* GetTrackInfoReferenceTable() const;
-        Util::ReferenceTable* GetNoteInfoReferenceTable() const;
-
-        TrackInfo* GetTrackInfo(u32 index) const;
-        NoteInfo* GetNoteInfo(u32 index) const;
-
         Util::Reference toWaveSoundInfo;
         Util::Reference toTrackInfoReferenceTable;
         Util::Reference toNoteInfoReferenceTable;
+
+        const WaveSoundInfo& GetWaveSoundInfo() const;
+
+        const Util::ReferenceTable& GetTrackInfoReferenceTable() const;
+        const Util::ReferenceTable& GetNoteInfoReferenceTable() const;
+
+        u32 GetTrackCount() const {
+            return GetTrackInfoReferenceTable().count;
+        }
+
+        u32 GetNoteCount() const {
+            return GetNoteInfoReferenceTable().count;
+        }
+
+        const TrackInfo& GetTrackInfo(u32 index) const;
+        const NoteInfo& GetNoteInfo(u32 index) const;
     };
     static_assert(sizeof(WaveSoundData) == 0x18);
 
     struct WaveSoundInfo {
+        Util::BitFlag optionParameter;
 
         u8 GetPan() const;
         s8 GetSurroundPan() const;
-        f32 GetPitch() const;
+        float GetPitch() const;
         void GetSendValue(u8* mainSend, u8* fxSend, u8 fxSendCount) const;
-        AdshrCurve* GetAdshrCurve() const;
+        const AdshrCurve& GetAdshrCurve() const;
         u8 GetLpfFreq() const;
         u8 GetBiquadType() const;
         u8 GetBiquadValue() const;
-
-        Util::BitFlag optionParameter;
     };
     static_assert(sizeof(WaveSoundInfo) == 0x4);
 
     struct NoteEvent;
     struct TrackInfo {
-
-        Util::ReferenceTable* GetNoteEventReferenceTable() const;
-        NoteEvent* GetNoteEvent(u32 index) const;
-
         Util::Reference toNoteEventReferenceTable;
+
+        const Util::ReferenceTable& GetNoteEventReferenceTable() const;
+
+        u32 GetNoteEventCount() const {
+            return GetNoteEventReferenceTable().count;
+        }
+
+        const NoteEvent& GetNoteEvent(u32 index) const;
     };
     static_assert(sizeof(TrackInfo) == 0x8);
 
+    struct NoteEvent {
+        u8 _0[8];
+    };
+    static_assert(sizeof(NoteEvent) == 0x8);
+
     struct NoteInfo {
-        
+        u32 waveIdTableIndex;
+        Util::BitFlag optionParameter;
+
         u8 GetOriginalKey() const;
         u8 GetVolume() const;
         u8 GetPan() const;
         u8 GetSurroundPan() const;
-        f32 GetPitch() const;
-        f32 GetSendValue(u8*, u8**, u8) const;
-        AdshrCurve* GetAdshrCurve() const;
-
-        u32 waveIdTableIndex;
-        Util::BitFlag optionParameter;
+        float GetPitch() const;
+        void GetSendValue(u8* mainSend, u8** fxSend, u8 fxSendCount) const;
+        const AdshrCurve& GetAdshrCurve() const;
     };
     static_assert(sizeof(NoteInfo) == 0x8);
 };
