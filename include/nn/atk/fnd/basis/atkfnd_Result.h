45d0b0bd9131d6c22b595a57ec959c953e2471ef
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-27 16:52:19 -0400

    atk: Match `FileStreamImpl::ReadDirect`

--- include/nn/atk/fnd/basis/atkfnd_Result.h
+++ include/nn/atk/fnd/basis/atkfnd_Result.h
@@ -4,17 +4,20 @@
 
 namespace nn::atk::detail::fnd {
 enum FndResultType {
+    FndResultType_ErrorFlagMask = 1 << 31,
+    FndResultType_CategoryMask = 0b1111111 << 24,
     FndResultType_CodeMask = 0xffffff,
-    FndResultType_CategoryMask = 0x7f000000,
-    FndResultType_ErrorFlagMask = ~(FndResultType_CategoryMask | FndResultType_CodeMask),
 
-    FndResultType_CategorySystem = 0x0,
-    FndResultType_CategoryBitOffset = 0x18,
-    FndResultType_CategoryIo = 0x1000000,
-
+    FndResultType_CategoryBitOffset = 24,
     FndResultType_ErrorFlag = FndResultType_ErrorFlagMask,
+
+    FndResultType_CategorySystem = 0,
+    FndResultType_CategoryIo = 1 << FndResultType_CategoryBitOffset,
     
-    FndResultType_Failed = FndResultType_ErrorFlag,
+    FndResultType_True = FndResultType_CategorySystem,
+    FndResultType_False,
+
+    FndResultType_Failed = FndResultType_ErrorFlag | FndResultType_CategorySystem,
     FndResultType_NotInitialized,
     FndResultType_NotSupported,
     FndResultType_NotOpened,
@@ -22,17 +25,52 @@ enum FndResultType {
     FndResultType_InvalidArgument,
     FndResultType_InvalidStatus,
 
-    FndResultType_IoError = -0x7f000000,
+    FndResultType_IoError = FndResultType_ErrorFlag | FndResultType_CategoryIo,
     FndResultType_IoFileNotFound,
     FndResultType_IoInvalidAccess,
     FndResultType_IoTargetLocked,
-
-    FndResultType_True = 0,
-    FndResultType_False,
 };
 
-struct FndResult {
+class FndResult {
+public:
+    FndResult() = default;
+    
+    explicit FndResult(u32 value)
+        : value{value} {};
+
+    explicit FndResult(FndResultType value)
+        : value(value) {};
+
+    bool IsSucceeded() const {
+        return !IsFailed();
+    }
+
+    bool IsTrue() const {
+        return value == FndResultType_True;
+    }
+
+    bool IsFalse() const {
+        return value == FndResultType_False;
+    }
+
+    bool IsFailed() const {
+        return value == FndResultType_Failed;
+    }
+
+    const char* ToString();
+
+    void PrintResult();
+
+    explicit operator u32() const { 
+        return value; 
+    }
+
+    explicit operator FndResultType() const {
+        return FndResultType(value);
+    }
+private:
     u32 value;
+
 };
 static_assert(sizeof(FndResult) == 0x4);
 } // namespace nn::atk::detail::fnd
