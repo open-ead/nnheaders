16f93f4c78cdcab4815424d759ccc526b3ebeda0
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-27 14:19:33 -0400

    atk: Match `FileStreamImpl::Read`
    Also reorganized class to better match DWARF

--- include/nn/atk/fnd/io/atkfnd_Stream.h
+++ include/nn/atk/fnd/io/atkfnd_Stream.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include <nn/types.h>
+#include <nn/util.h>
 
 #include <nn/atk/fnd/basis/atkfnd_Config.h>
 #include <nn/atk/fnd/basis/atkfnd_Result.h>
 
 namespace nn::atk::detail::fnd {
 class Stream {
+    NN_NO_COPY(Stream);
 public:
+    static const position_t InvalidPosition = 0xffffffff;
+    static const size_t InvalidSize = 0xffffffff;
+
     enum SeekOrigin {
         SeekOrigin_Begin,
         SeekOrigin_End,
@@ -15,20 +20,24 @@ public:
     };
 
     virtual ~Stream();
-    
+
+protected:
+    Stream();
+
+public:
     virtual void Close() = 0;
     virtual bool IsOpened() const = 0;
     
-    virtual size_t Read(void* buf, size_t length, FndResult* result) = 0;
-    virtual size_t Write(const void* buf, size_t length, FndResult* result) = 0;
-    virtual FndResult Seek(position_t offset, SeekOrigin origin) = 0;
+    virtual size_t Read(void* buf, size_t length, FndResult* result);
+    virtual size_t Write(const void* buf, size_t length, FndResult* result);
+    virtual FndResult Seek(position_t offset, SeekOrigin origin);
     
-    virtual position_t GetCurrentPosition() const = 0;
-    virtual size_t GetSize() const = 0;
+    virtual position_t GetCurrentPosition() const;
+    virtual size_t GetSize() const;
     
-    virtual bool CanRead() const = 0;
-    virtual bool CanWrite() const = 0;
-    virtual bool CanSeek() const = 0;
+    virtual bool CanRead() const;
+    virtual bool CanWrite() const;
+    virtual bool CanSeek() const;
 };
 static_assert(sizeof(Stream) == 0x8);
 } // namespace nn::atk::detail::fnd
