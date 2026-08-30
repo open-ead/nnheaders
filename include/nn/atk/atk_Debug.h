ba9d0ff2e194276da4784483a8bc5385fb4e2abd
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-30 19:12:21 -0400

    atk: Match `detail::Debug_GetWarningFlag`

--- include/nn/atk/atk_Debug.h
+++ include/nn/atk/atk_Debug.h
@@ -12,10 +12,8 @@ enum DebugWarningFlag {
     DebugWarningFlag_NotEnoughStrmchannel,
 };
 
-static u8 gWarningFlag = 0b11111;
-static const u32 gWarningBitFlags[6] = {0b11111, 0b00001, 0b00010, 0b00100, 0b01000, 0b10000};
-
-void Debug_SetWarningFlag(DebugWarningFlag warning, bool);
+void Debug_SetWarningFlag(DebugWarningFlag warning, bool enable);
+// extern u32 gWarningFlag;
 
 namespace detail {
 enum DebugSoundType {
@@ -31,7 +29,5 @@ DebugWarningFlag Debug_GetDebugWarningFlagFromSoundType(DebugSoundType type);
 char* Debug_GetSoundTypeString(DebugSoundType type);
 
 using DebugLogFunc = void(*)(char*);
-
-static DebugLogFunc g_DebugLogHookFunc;
 } // namespace nn::atk::detail
 } // namespace nn::atk
---
+++ src/NintendoWare/atk/detail/debug/atk_Debug.cpp
@@ -0,0 +1,64 @@
+#include <nn/atk/atk_Debug.h>
+
+namespace nn::atk {
+namespace {
+const u32 NotEnoughSeqSound     {0b00001};
+const u32 NotEnoughStrmSound    {0b00010};
+const u32 NotEnoughWaveSound    {0b00100};
+const u32 NotEnoughSeqTrack     {0b01000};
+const u32 NotEnoughStrmChannel  {0b10000};
+const u32 NotEnoughInstance     {0b11111};
+
+u32 gWarningFlag = NotEnoughInstance;
+
+u32 GetWarningBitFlag(DebugWarningFlag warning) {
+    u32 bitFlag {0};
+
+    switch (warning) {
+    case DebugWarningFlag_NotEnoughInstance:
+        bitFlag = NotEnoughInstance;
+        break;
+
+    case DebugWarningFlag_NotEnoughSeqsound:
+        bitFlag = NotEnoughSeqSound;
+        break;
+
+    case DebugWarningFlag_NotEnoughStrmsound:
+        bitFlag = NotEnoughStrmSound;
+        break;
+
+    case DebugWarningFlag_NotEnoughWavesound:
+        bitFlag = NotEnoughWaveSound;
+        break;
+    
+    case DebugWarningFlag_NotEnoughSeqtrack:
+        bitFlag = NotEnoughSeqTrack;
+        break;
+
+    case DebugWarningFlag_NotEnoughStrmchannel:
+        bitFlag = NotEnoughStrmChannel;
+        break;
+    }
+
+    return bitFlag;
+}
+} // anonymous namespace
+
+// NON_MATCHING
+void Debug_SetWarningFlag(DebugWarningFlag warning, bool enable) {
+    u32 bitFlag {GetWarningBitFlag(warning)};
+
+    if (enable)
+        gWarningFlag = bitFlag | gWarningFlag;
+}
+
+namespace detail {
+DebugLogFunc g_DebugLogHookFunc;
+
+bool Debug_GetWarningFlag(DebugWarningFlag warning) {
+    u32 bitFlag {GetWarningBitFlag(warning)};
+
+    return (bitFlag & ~gWarningFlag) == 0;
+}
+} // namespace nn::atk::detail 
+} // namespace nn::atk
