54abcf8d5b095376ad73083339ca5ac08802310b
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-08-31 14:41:30 -0400

    atk: Match `WaveArchiveFile::FileHeader::GetFileBlockSize`
    This matches all exported functions in `WaveArchiveFile`

--- src/NintendoWare/atk/detail/atk_WaveArchiveFile.cpp
+++ src/NintendoWare/atk/detail/atk_WaveArchiveFile.cpp
@@ -3,6 +3,30 @@
 #include <nn/atk/atk_ElementType.h>
 
 namespace nn::atk::detail {
+const WaveArchiveFile::InfoBlock* WaveArchiveFile::FileHeader::GetInfoBlock() const { // 28
+    return util::ConstBytePtr(this, GetInfoBlockOffset()).Get<InfoBlock>();
+}
+
+const WaveArchiveFile::FileBlock* WaveArchiveFile::FileHeader::GetFileBlock() const { // 34
+    return util::ConstBytePtr(this, GetFileBlockOffset()).Get<FileBlock>();
+}
+
+u32 WaveArchiveFile::FileHeader::GetInfoBlockSize() const {
+    return GetReferenceBy(ElementType_WaveArchiveFile_InfoBlock)->size;
+}
+
+u32 WaveArchiveFile::FileHeader::GetFileBlockSize() const {
+    return GetReferenceBy(ElementType_WaveArchiveFile_FileBlock)->size;
+}
+
+u32 WaveArchiveFile::FileHeader::GetInfoBlockOffset() const {
+    return GetReferenceBy(ElementType_WaveArchiveFile_InfoBlock)->offset;
+}
+
+u32 WaveArchiveFile::FileHeader::GetFileBlockOffset() const {
+    return GetReferenceBy(ElementType_WaveArchiveFile_FileBlock)->offset;
+}
+
 const Util::ReferenceWithSize* WaveArchiveFile::FileHeader::GetReferenceBy(u16 typeId) const {
     for (int i {0}; i < BlockCount; ++i) {
         if (toBlocks[i].IsValidTypeId(typeId)) {
@@ -11,25 +35,5 @@ const Util::ReferenceWithSize* WaveArchiveFile::FileHeader::GetReferenceBy(u16 t
     }
 
     return nullptr;
-}
-
-const WaveArchiveFile::InfoBlock* WaveArchiveFile::FileHeader::GetInfoBlock() const {
-    return util::ConstBytePtr(this, GetInfoBlockOffset()).Get<InfoBlock>();
-}
-
-u32 WaveArchiveFile::FileHeader::GetInfoBlockOffset() const {
-    return GetReferenceBy(ElementType_WaveArchiveFile_InfoBlock)->offset;
-}
-
-const WaveArchiveFile::FileBlock* WaveArchiveFile::FileHeader::GetFileBlock() const {
-    return util::ConstBytePtr(this, GetFileBlockOffset()).Get<FileBlock>();
-}
-
-u32 WaveArchiveFile::FileHeader::GetFileBlockOffset() const {
-    return GetReferenceBy(ElementType_WaveArchiveFile_FileBlock)->offset;
-}
-
-u32 WaveArchiveFile::FileHeader::GetInfoBlockSize() const {
-    return GetReferenceBy(ElementType_WaveArchiveFile_InfoBlock)->size;
 }
 } // namespace nn::atk::detail
