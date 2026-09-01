6cd33ed317c146531dd91957496ff1759343b716
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-01 13:18:55 -0400

    atk: Match `FileStreamProxy` destructor

--- src/NintendoWare/atk/fnd/io/atkfnd_FileStreamProxy.cpp
+++ src/NintendoWare/atk/fnd/io/atkfnd_FileStreamProxy.cpp
@@ -3,4 +3,10 @@
 namespace nn::atk::detail::fnd {
 FileStreamProxy::FileStreamProxy(FileStream* fileStream, position_t offset, size_t fileSize)
     : m_pFileStream{fileStream}, m_Offset{offset}, m_FileSize{fileSize} {}
+
+FileStreamProxy::~FileStreamProxy() {
+    m_Offset = 0;
+    m_FileSize = 0;
+    m_pFileStream = nullptr;
+}
 } // namespace nn::atk::detail::fnd
