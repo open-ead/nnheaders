c0ec7b2545acaf9e13fed53d274cbe342deb222f
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-03 12:23:36 -0400

    atk: Add functions for `ThreadInfoRecorder`

--- include/nn/atk/atk_ThreadInfoReader.h
+++ include/nn/atk/atk_ThreadInfoReader.h
@@ -8,9 +8,36 @@ namespace nn::atk {
 namespace detail {
 class ThreadInfoRecorder {
 public:
+    static const size_t BufferMinimumSize {};
+
+    ThreadInfoRecorder();
+
+    void Initialize(void* buffer, size_t bufferSize) {
+        m_Buffer = buffer;
+        m_BufferSize = bufferSize;
+    }
+
+    int GetRecordedFrameCount() const { return m_RecordFrameCount; }
+
+    const void* GetRingBufferHeadAddress() const {
+        // TODO
+        return nullptr;
+    }
+
+    void MoveToNextFrame() {
+        // TODO
+    }
+
+    void RecordFooter() {
+        // TODO
+    }
+    
     util::IntrusiveListNode m_List;
 
 private:
+    void* AllocateInfoBuffer([[maybe_unused]] u32 count, [[maybe_unused]] size_t size) { return nullptr; }
+    void* AllocateBuffer([[maybe_unused]] u32 count, [[maybe_unused]] size_t size1, [[maybe_unused]]size_t size2) { return nullptr; }
+
     void* m_Buffer;
     size_t m_BufferSize;
     size_t m_WritePosition;
@@ -19,10 +46,10 @@ private:
     std::atomic_bool m_IsAllocationFailed;
 };
 static_assert(sizeof(ThreadInfoRecorder) == 0x38);
-} // namespace nn::atk::detail
+}  // namespace detail
 
 using SoundThreadInfoRecorder = detail::ThreadInfoRecorder;
 using SoundThreadInfoRecorderList = util::IntrusiveList<SoundThreadInfoRecorder, 
                                         util::IntrusiveListMemberNodeTraits<SoundThreadInfoRecorder,
                                             &SoundThreadInfoRecorder::m_List>>;
-} // namespace nn::atk
+}  // namespace nn::atk
