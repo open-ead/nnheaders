f53be4b34b969db5b94d5ade82b14a68b1072487
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-03 12:10:16 -0400

    atk: Match `LowLevelVoice::AppendWaveBuffer`

--- src/NintendoWare/atk/detail/voice/atk_LowLevelVoice.cpp
+++ src/NintendoWare/atk/detail/voice/atk_LowLevelVoice.cpp
@@ -4,4 +4,15 @@ namespace nn::atk::detail {
 
 LowLevelVoice::LowLevelVoice() = default;
 
+void LowLevelVoice::AppendWaveBuffer(WaveBuffer* waveBuffer) {
+    waveBuffer->next = nullptr;
+    waveBuffer->status = WaveBuffer::Status_Wait;
+    if (m_WaveBufferListEnd == nullptr)
+        m_WaveBufferListBegin = waveBuffer;
+    else
+        m_WaveBufferListEnd->next = waveBuffer;
+
+    m_WaveBufferListEnd = waveBuffer;
+}
+
 }  // namespace nn::atk::detail
