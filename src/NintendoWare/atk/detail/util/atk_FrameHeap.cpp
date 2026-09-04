b2298784599467f8b237e4665644ab2f96c88056
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-04 14:33:25 -0400

    atk: Match `FrameHeap::Destroy`

--- src/NintendoWare/atk/detail/util/atk_FrameHeap.cpp
+++ src/NintendoWare/atk/detail/util/atk_FrameHeap.cpp
@@ -4,4 +4,18 @@ namespace nn::atk::detail {
 
 FrameHeap::FrameHeap() = default;
 
+bool FrameHeap::Create(void* startAddress, size_t size) {
+    Destroy();
+
+}
+
+void FrameHeap::Destroy() {
+    if (IsValid()) {
+        ClearSection();
+        m_pHeap->Free(fnd::FrameHeapImpl::FreeAllMode);
+        m_pHeap->Destroy();
+        m_pHeap = nullptr;
+    }
+}
+
 }  // namespace nn::atk::detail
