ea787d43d65edf6acd18ed8bb0b4d989a5107638
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-05 17:21:04 -0400

    atk: Match `PoolImpl::DestroyImpl`

--- src/NintendoWare/atk/detail/util/atk_InstancePool.cpp
+++ src/NintendoWare/atk/detail/util/atk_InstancePool.cpp
@@ -23,20 +23,34 @@ int PoolImpl::CreateImpl(void* buffer, size_t size, size_t objSize, size_t align
     return objectCount;
 }
 
+void PoolImpl::DestroyImpl() {
+    void* begin{util::BytePtr(m_pBuffer).Get()};
+    void* end{util::BytePtr(m_pBuffer, m_BufferSize).Get()};
+    
+    PoolImpl* ptr{m_pNext};
+    PoolImpl* prev{this};
+
+    for (; ptr != nullptr; ptr = ptr->m_pNext) {
+        if (ptr >= begin && ptr < end)
+            prev->m_pNext = ptr->m_pNext;
+        else
+            prev = ptr;
+    }
+}
+
 void* PoolImpl::AllocImpl() {
     PoolImpl* head{m_pNext};
 
-    if (head != nullptr) {
+    if (head != nullptr)
         m_pNext = head->m_pNext;
-    }
 
     return head;
 }
 
 int PoolImpl::CountImpl() const {
     int count {0};
-    
-    for (PoolImpl* ptr {m_pNext}; ptr != nullptr; ptr = ptr->m_pNext)
+
+    for (PoolImpl* ptr{m_pNext}; ptr != nullptr; ptr = ptr->m_pNext)
         ++count;
 
     return count;
