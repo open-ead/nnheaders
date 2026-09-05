1ee47293861663a615fe3c9d967feeaa558dccfa
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-05 17:58:31 -0400

    atk: Match `MemoryFileStream::Close`

--- src/NintendoWare/atk/detail/util/atk_MemoryFileStream.cpp
+++ src/NintendoWare/atk/detail/util/atk_MemoryFileStream.cpp
@@ -5,8 +5,15 @@ namespace nn::atk::detail {
 MemoryFileStream::MemoryFileStream(const void* buffer, size_t size)
     : m_pBuffer(buffer), m_Size(size), m_Position(0) {}
 
-fnd::FndResult MemoryFileStream::Open(const char* filePath, AccessMode openMode) {
+fnd::FndResult MemoryFileStream::Open([[maybe_unused]] const char* filePath,
+                                      [[maybe_unused]] AccessMode openMode) {
     return fnd::FndResult{fnd::FndResultType_Failed};
+}
+
+void MemoryFileStream::Close() {
+    m_pBuffer = nullptr;
+    m_Size = 0;
+    m_Position = 0;
 }
 
 }  // namespace nn::atk::detail
