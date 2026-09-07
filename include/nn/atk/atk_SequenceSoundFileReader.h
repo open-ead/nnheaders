aec77ccc53f17f67775d7831746b1011a3118c6c
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-07 12:18:17 -0300

    atk: Match `SequenceSoundFileReader` constructor

--- include/nn/atk/atk_SequenceSoundFileReader.h
+++ include/nn/atk/atk_SequenceSoundFileReader.h
@@ -3,21 +3,29 @@
 #include <nn/atk/atk_SequenceSoundFile.h>
 
 namespace nn::atk::detail {
+
 class SequenceSoundFileReader {
 public:
-    constexpr static s32 SignatureFile = 0x51455346; // FSEQ
+    static const u32 SignatureFile{0x51455346};  // FSEQ
 
-    explicit SequenceSoundFileReader(void* sequenceFile);
+    explicit SequenceSoundFileReader(const void* sequenceFile);
 
-    void* GetSequenceData();
+    bool IsAvailable() const { return m_pHeader != nullptr; }
+
+    const void* GetSequenceData();
 
     bool GetOffsetByLabel(const char* label, u32* offsetPtr) const;
     bool GetLabelByOffset(u32 offset) const;
 
+    int GetLabelCount() const { return m_pLabelBlockBody->GetLabelCount(); }
+
+    const char* GetLabel(int index) const { return m_pLabelBlockBody->GetLabel(index); }
+
 private:
-    SequenceSoundFile::FileHeader* m_pHeader;
-    SequenceSoundFile::DataBlockBody* m_pDataBlockBody;
-    SequenceSoundFile::LabelBlockBody* m_pLabelBlockBody;
+    const SequenceSoundFile::FileHeader* m_pHeader{};
+    const SequenceSoundFile::DataBlockBody* m_pDataBlockBody{};
+    const SequenceSoundFile::LabelBlockBody* m_pLabelBlockBody{};
 };
 static_assert(sizeof(SequenceSoundFileReader) == 0x18);
-} // namespace nn::atk::detail
+
+}  // namespace nn::atk::detail
