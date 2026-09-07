0acf4a04564efe9f530f26a76b8521ac59696428
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-07 17:24:50 -0300

    atk: Reorganize `atk_Global.h`
    Moved stuff around to better match DWARF

--- include/nn/atk/atk_Config.h
+++ include/nn/atk/atk_Config.h
@@ -7,4 +7,11 @@
 namespace nn::atk {
 using position_t = detail::fnd::position_t;
 
+const int StreamChannelCount{16};
+const u32 StreamTrackCount{8};
+
+const int StreamDataLoadTaskMax{32};
+
+static const size_t DataBlockSizeMarginSamples{0x480};
+
 } // namespace nn::atk
