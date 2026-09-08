cd38ddaf3546a035768de61c21ce5cedcac11d4e
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-08 15:27:53 -0300

    atk: Match `Channel::CallChannelCallback`

--- src/NintendoWare/atk/detail/voice/atk_Channel.cpp
+++ src/NintendoWare/atk/detail/voice/atk_Channel.cpp
@@ -22,4 +22,13 @@ Channel::~Channel() {
     DisposeCallbackManager::GetInstance()->UnregisterDisposeCallback(&m_Disposer);
 }
 
+void Channel::CallChannelCallback(ChannelCallbackStatus status) {
+    if (m_Callback == nullptr)
+        return;
+
+    m_Callback(this, status, m_CallbackData);
+    m_Callback = nullptr;
+    m_CallbackData = nullptr;
+}
+
 }  // namespace nn::atk::detail::driver
