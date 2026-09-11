6127a716fc96361b888bdff410ffb78013bcce1f
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-11 15:13:01 -0300

    atk: Match `BasicSound::SetPlayerPriority`

--- src/NintendoWare/atk/detail/atk_BasicSound.cpp
+++ src/NintendoWare/atk/detail/atk_BasicSound.cpp
@@ -1,5 +1,7 @@
 #include <nn/atk/atk_BasicSound.h>
 
+#include <nn/atk/atk_SoundPlayer.h>
+
 namespace nn::atk::detail {
 
 // NON_MATCHING on versions lower than 4.0.0
@@ -9,4 +11,12 @@ void BasicSound::StartPrepared() {
     m_StartFlag = true;
 }
 
+void BasicSound::SetPlayerPriority(int priority) {
+    m_Priority = priority;
+    
+    if (m_pSoundPlayer != nullptr)
+        m_pSoundPlayer->detail_SortPriorityList(this);
+    
+    OnUpdatePlayerPriority();
+}
 }  // namespace nn::atk::detail
