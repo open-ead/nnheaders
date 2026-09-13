2df982e8df59ef3d806a1d007e46821f2d7c4388
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-12 21:26:41 -0300

    atk: Match `BasicSound::GetFxSend`

--- src/NintendoWare/atk/detail/atk_BasicSound.cpp
+++ src/NintendoWare/atk/detail/atk_BasicSound.cpp
@@ -430,6 +430,10 @@ void BasicSound::SetFxSend(AuxBus bus, float send) {
     m_CommonParam.send[bus + 1L] = send;
 }
 
+float BasicSound::GetFxSend(AuxBus bus) const {
+    return m_CommonParam.send[bus + 1L];
+}
+
 void BasicSound::SetId(u32 id) {
     m_Id = id;
 }
