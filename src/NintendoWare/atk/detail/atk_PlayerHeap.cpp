d95709d0c69ba5c22f5104b038a66e6604fb6659
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-14 09:22:42 -0300

    atk: Match `PlayerHeap::Create`

--- src/NintendoWare/atk/detail/atk_PlayerHeap.cpp
+++ src/NintendoWare/atk/detail/atk_PlayerHeap.cpp
@@ -1,5 +1,8 @@
 #include <nn/atk/atk_PlayerHeap.h>
 
+#include <nn/atk/atk_Util.h>
+#include <nn/atk/fnd/os/atkfnd_Thread.h>
+
 namespace nn::atk::detail {
 
 PlayerHeap::PlayerHeap() = default;
@@ -12,6 +15,22 @@ PlayerHeap::~PlayerHeap() {
 void PlayerHeap::Destroy() {
     Clear();
     m_pAllocAddress = nullptr;
+}
+
+bool PlayerHeap::Create(void* startAddress, size_t size) {
+    Util::IsValidMemoryForDsp(startAddress, size);
+
+    void* endAddress{util::BytePtr(startAddress, static_cast<ptrdiff_t>(size)).Get()};
+    startAddress = util::BytePtr(startAddress).AlignUp(fnd::Thread::StackAlignment).Get();
+
+    if (startAddress <= endAddress) {
+        m_pStartAddress = startAddress;
+        m_pEndAddress = endAddress;
+        m_pAllocAddress = startAddress;
+        return true;
+    }
+
+    return false;
 }
 
 }  // namespace nn::atk::detail
