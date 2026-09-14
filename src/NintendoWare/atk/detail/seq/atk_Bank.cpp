02c3b2ab59f0f416d2329524b4119f3b2264254a
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-14 15:47:00 -0300

    atk: Match `Bank::CalcChannelVelocityVolume`

--- src/NintendoWare/atk/detail/seq/atk_Bank.cpp
+++ src/NintendoWare/atk/detail/seq/atk_Bank.cpp
@@ -1,9 +1,19 @@
 #include <nn/atk/atk_Bank.h>
 
+namespace {
+const float VelocityMaxR{1.0f / 127.0f};
+
+const float InstrumentVolumeCriterionR{1.0f / 127.0f};
+}
+
 namespace nn::atk::detail::driver {
 
 Bank::Bank() = default;
 
 Bank::~Bank() = default;
+
+float Bank::CalcChannelVelocityVolume(u8 velocity) {
+    return static_cast<float>(velocity) * VelocityMaxR;
+}
 
 }  // namespace nn::atk::detail::driver
