de6e394f0131b90b6ccae3948122a0b89381f8eb
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-14 09:06:01 -0300

    atk: Match `PlayerHeap` constructor

--- include/nn/atk/atk_PlayerHeap.h
+++ include/nn/atk/atk_PlayerHeap.h
@@ -12,45 +12,59 @@ namespace detail {
 
 class PlayerHeap;
 class CallbackNode {
-private:
-    friend PlayerHeap;
+public:
+    void SetCallback(SoundMemoryAllocatable::DisposeCallback callback) { m_Callback = callback; }
+    void SetCallbackArg(void* arg) { m_CallbackArg = arg; }
+
+    SoundMemoryAllocatable::DisposeCallback GetCallback() { return m_Callback; }
+    void* GetCallbackArg() { return m_CallbackArg; }
 
     util::IntrusiveListNode m_Link;
+
+private:
     SoundMemoryAllocatable::DisposeCallback m_Callback;
     void* m_CallbackArg;
 };
 static_assert(sizeof(CallbackNode) == 0x20);
 
-class PlayerHeap : SoundMemoryAllocatable {
+class PlayerHeap : public SoundMemoryAllocatable {
 public:
-    using CallbackList = util::IntrusiveList<
-        CallbackNode, util::IntrusiveListMemberNodeTraits<CallbackNode, &CallbackNode::m_Link>>;
-
     PlayerHeap();
     ~PlayerHeap() override;
 
-    void Destroy();
+    void AttachSoundPlayer(SoundPlayer* player) { m_pPlayer = player; }
 
     bool Create(void* startAddress, size_t size);
+    void Destroy();
+
+    void* Allocate(size_t size) override;
+    void* Allocate(size_t size, DisposeCallback callback, void* callbackArg) override;
+
+    size_t GetAllocateSize(size_t size, bool needMemoryPool) override;
 
     void Clear();
 
-    void* Allocate(size_t size) override;
-    void* Allocate(size_t size, SoundMemoryAllocatable::DisposeCallback callback,
-                   void* callbackArg) override;
+    bool IsValid() const;
 
-    size_t GetAllocateSize(size_t size, bool needMemoryPool) override;
     size_t GetFreeSize() const;
 
-private:
-    friend SoundPlayer;
+    enum State { State_Constructed, State_TaskRegistered, State_TaskFinished, State_Destructed };
 
-    SoundPlayer* m_pPlayer;
-    void* m_pStartAddress;
-    void* m_pEndAddress;
-    void* m_pAllocAddress;
-    u8 m_State;
+    State GetState() const { return static_cast<State>(m_State); }
+    void SetState(State state) { m_State = static_cast<u8>(state); }
+
+private:
+    using CallbackList = util::IntrusiveList<
+        CallbackNode, util::IntrusiveListMemberNodeTraits<CallbackNode, &CallbackNode::m_Link>>;
+
+    SoundPlayer* m_pPlayer{};
+    void* m_pStartAddress{};
+    void* m_pEndAddress{};
+    void* m_pAllocAddress{};
+    u8 m_State{State_Constructed};
     u8 m_Padding[3];
+
+public:
     util::IntrusiveListNode m_Link;
     CallbackList m_CallbackList;
 };
--- src/NintendoWare/atk/detail/atk_PlayerHeap.cpp
+++ src/NintendoWare/atk/detail/atk_PlayerHeap.cpp
@@ -1,1 +1,7 @@
+#include <nn/atk/atk_PlayerHeap.h>
 
+namespace nn::atk::detail {
+
+PlayerHeap::PlayerHeap() = default;
+
+}  // namespace nn::atk::detail
